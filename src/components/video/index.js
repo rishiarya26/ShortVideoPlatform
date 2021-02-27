2322cb9f8db8f60fcdb955eb0a234c750e38f624
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-02-27 19:15:45 +0530

    all static data populated

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -2,10 +2,44 @@ import React, { useRef, useState, useEffect } from 'react';
 import VideoFooter from '../videofooter/index';
 import VideoSidebar from '../videosidebar/index';
 
+
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+  
+    function handleResize() {
+     
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    
+    window.addEventListener("resize", handleResize);
+    
+    
+    handleResize();
+    
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+
+
 function Video(props) {
   const [playing, setPlaying] = useState(true);
   const [clicked, setClicked] = useState(true);
   const videoRef = useRef(null);
+  const size = useWindowSize();
 
   const handleVideoPress = () => {
     if (playing) {
@@ -49,10 +83,10 @@ function Video(props) {
 
     <div className="video_card relative w-full h-full scroll-snap-start ">
 
-      <video loop ref={videoRef} onClick={handleVideoPress} className="vdo_player" width={412} height={731}>
+      <video loop ref={videoRef} onClick={handleVideoPress} className="vdo_player" width={size.width} height={size.height}>
         <source src="https://media.charmboard.com/images/demo_videos/116281312_735041643986642_3875123162107059152_n.mp4" type="video/mp4" />
       </video>
-      <VideoSidebar />
+     <VideoSidebar />
       <VideoFooter />
     </div>
   );
--- src/components/videofooter/index.js
+++ src/components/videofooter/index.js
@@ -5,11 +5,10 @@ function VideoFooter(props) {
   return (
     <div className="videoFooter relative flex bottom-24 text-white ml-5">
       <div className="videoFooter__text">
-        <h3>Username</h3>
+        <h3 className="font-bold">Username</h3>
         <p>Nature at its best #nature #green</p>
-        <div className="">
-          <MusicNoteIcon className=" float-left" />
-          <div className="">
+        <div>
+          <svg className=" float-left" width="24" height="24" viewBox="6 0 24 24" fill="none"><path class="st0" fill="white" d="M12,3v10.6c-0.6-0.3-1.3-0.6-2-0.6c-2.2,0-4,1.8-4,4s1.8,4,4,4s4-1.8,4-4V7h4V3H12z"/></svg>
             <Ticker mode="smooth" className="">
               {({ index }) => (
                 <>
@@ -17,12 +16,9 @@ function VideoFooter(props) {
                 </>
               )}
             </Ticker>
-          </div>
         </div>
       </div>
-      <div
-        className="videoFooter__record flex items-center justify-center animate-spin-slow absolute w-12 h-12 bottom-0 right-8 rounded-full overflow-hidden profile-bg bg-no-repeat bg-contain"
-      >
+      <div className="videoFooter__record flex items-center justify-center animate-spin-slow absolute w-12 h-12 bottom-0 right-8 rounded-full overflow-hidden profile-bg bg-no-repeat bg-contain">
         <img
           className="w-3/5 rounded-full"
           src="https://assets2.charmboard.com/pro/images/104578166157776556785/1578291929591.jpeg?tr=w-200,h-200,z-0.75,fo-face,c-thumb,pr-true,q-70,g-face"
