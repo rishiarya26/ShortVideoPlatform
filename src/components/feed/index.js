6690d494df4d0aa14418425931ca0c9b0e372624
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-03-02 17:50:38 +0530

    swiper integrted

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,13 +1,28 @@
 import Video from '../video';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 export default function Feed() {
   return (
-    <div className="grid h-full">
-      <div className="relative overflow-scroll w-full max-w-screen-sm Video_sheet">
-        <Video />
-        <Video />
-        <Video />
-      </div>
-    </div>
+    <Swiper
+    spaceBetween={50}
+    direction='vertical'
+    draggable='true'
+    calculateHeight='true'
+    >
+      <SwiperSlide><Video /></SwiperSlide>
+      <SwiperSlide><Video /></SwiperSlide>
+      <SwiperSlide><Video /></SwiperSlide>
+      <SwiperSlide><Video /></SwiperSlide>
+      ...
+    </Swiper>
+
+    // <div className="grid h-full">
+    //   <div className="relative overflow-scroll w-full max-w-screen-sm Video_sheet">
+    //     <Video />
+    //     <Video />
+    //     <Video />
+    //   </div>
+    // </div>
   );
 }
--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
+
 import VideoFooter from '../videofooter/index';
 import VideoSidebar from '../videosidebar/index';
 
