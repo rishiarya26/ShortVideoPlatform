b1a87e7c21f1294a11458aafd8be750054d9f6e5
Ankit Agarwal <ankit.agarwal@ZL-MU-MT-MAC100.local>
2021-03-03 18:27:47 +0530

    add a sample

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import Video from '../video';
+import { getHomeFeed } from '../../sources/home';
+import ComponentStateHandler, { useFetcher } from '../commons/component-state-handler';
 
 export default function Feed() {
+  const [items, setItems] = useState({});
+  const dataFetcher = () => getHomeFeed();
+  const onDataFetched = data => {
+    console.log(data);
+    setItems(data, () => {
+      console.log(items);
+    });
+  };
+  const [fetchState] = useFetcher(dataFetcher, onDataFetched);
   return (
-    <div className="grid h-full">
-      <div className="relative overflow-scroll w-full max-w-screen-sm Video_sheet">
-        <Video />
-        <Video />
-        <Video />
+    <ComponentStateHandler
+      state={fetchState}
+    >
+      <div className="grid h-full">
+        <div className="relative overflow-scroll w-full max-w-screen-sm Video_sheet">
+          <Video />
+          <Video />
+          <Video />
+        </div>
       </div>
-    </div>
+    </ComponentStateHandler>
   );
 }
