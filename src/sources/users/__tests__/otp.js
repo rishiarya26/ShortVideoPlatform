5c002c382979e3810b24083b67797d7e4e42783f
hariom <hariom.srivastava@charmboard.com>
2021-03-15 15:35:24 +0530

    change feed seed and transform

--- src/sources/users/__tests__/otp.js
+++ src/sources/users/__tests__/otp.js
@@ -1,30 +1,30 @@
 import { get } from 'network';
-import {
-  getHomeFeed
-} from '../index';
-import { forYouFeed, forYouFeedFailure } from '../../../mock/seeds/feed';
-import { feed } from '../../factories/feed';
+import { srSendOtp } from '../index';
+import { getApiBasePath } from '../../../config';
+import { forYouFeed, forYouFeedFailure } from '../../../mock/seeds/otp';
+import { otp } from '../../factories/otp';
 
 jest.mock('network', () => ({
   get: jest.fn()
 }));
+const apiPath = `${getApiBasePath('hipi')}/v1/shorts/sendOtp`;
 
-describe('source for feed', () => {
+describe('source for otp', () => {
   it('should return the expected response for success state', async () => {
     get.mockResolvedValueOnce({ data: forYouFeed });
-    const data = await getHomeFeed();
+    const data = await srSendOtp();
     expect(get).toBeCalledTimes(1);
-    expect(get).toBeCalledWith('https://mobiletest.charmboard.com/v3.6/demo/hipifeed/1');
-    expect(data).toMatchObject(feed);
+    expect(get).toBeCalledWith(apiPath);
+    expect(data).toMatchObject(otp);
   });
 
   it('should return the expected response for failure state', async () => {
     get.mockRejectedValueOnce({ data: forYouFeedFailure });
     try {
-      await getHomeFeed();
+      await srSendOtp();
     } catch (e) {
       expect(get).toBeCalledTimes(1);
-      expect(get).toBeCalledWith('https://mobiletest.charmboard.com/v3.6/demo/hipifeed/1');
+      expect(get).toBeCalledWith(apiPath);
       expect(e).toMatchObject({
         status: 'fail',
         message: 'something went wrong',
