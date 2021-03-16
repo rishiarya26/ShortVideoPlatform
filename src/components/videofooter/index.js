147e0a3bd3abea471688c1bee38d1f9aeaaf1960
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-03-16 20:03:36 +0530

    replaced ticker update

--- src/components/videofooter/index.js
+++ src/components/videofooter/index.js
@@ -1,4 +1,5 @@
 import Marquee from '../commons/text-marquee';
+import Music from '../commons/svgicons/music';
 
 function VideoFooter() {
   return (
@@ -8,15 +9,7 @@ function VideoFooter() {
         <p>Nature at its best </p>
         <p>#nature #green #hash #tags</p>
         <div>
-          <svg
-            className="float-left"
-            width="24"
-            height="24"
-            viewBox="6 0 24 24"
-            fill="none"
-          >
-            <path className="st0" fill="white" d="M12,3v10.6c-0.6-0.3-1.3-0.6-2-0.6c-2.2,0-4,1.8-4,4s1.8,4,4,4s4-1.8,4-4V7h4V3H12z" />
-          </svg>
+         <Music/>
           <Marquee text="music title goes here" />
         </div>
       </div>
