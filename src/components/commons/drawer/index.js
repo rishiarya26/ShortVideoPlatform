5af53b3ae603767449a8d379c284f86ffdc933f5
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-03-19 12:14:31 +0530

    embed-cosmetic

--- src/components/commons/drawer/index.js
+++ src/components/commons/drawer/index.js
@@ -8,16 +8,16 @@ const Drawer = ({
     data-testid="drawer-container"
     className={`${visible ? 'h-3/4' : 'h-0'} 
     baseContainer z-10 fixed bottom-0 w-full overflow-hidden rounded-t-2xl bg-white
-    transition-all duration-1000`}
+    transition-all duration-500`}
   >
-    <div className="drawer-header w-full flex-row justify-between p-4">
+    <div className="drawer-header w-full flex-row justify-between p-6">
       <div className="drawer-title text-lg">
         {title}
         {' '}
         <div
           onClick={() => close()}
           alt="Close Icon"
-          className="w-6 h-6 absolute right-8"
+          className="w-6 h-6 absolute right-3 top-3"
           data-testid="drawer-close"
           role="presentation"
         >
