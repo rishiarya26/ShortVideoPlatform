93115db4d509885d5c6e311fead8bd968099557e
Ankit Agarwal <ankit.agarwal@ZL-MU-MT-MAC100.local>
2021-03-23 15:52:34 +0530

    wip

--- src/components/commons/head-meta/seo-meta.js
+++ src/components/commons/head-meta/seo-meta.js
@@ -7,7 +7,7 @@ const DEFAULT_SEO = {
   openGraph: {
     type: 'website',
     locale: 'en-IN',
-    url: 'https://hipi.com',
+    url: 'https://hipi-web.s3.ap-south-1.amazonaws.com/feed.html',
     title: 'hipi',
     description: 'watch videos on hipi',
     image:
@@ -22,38 +22,40 @@ const DEFAULT_SEO = {
   }
 };
 
-function isNewPage(prevProps, newProps) {
-  return (prevProps.path === newProps.path);
-}
+// function isNewPage(prevProps, newProps) {
+//   return (prevProps.path === newProps.path);
+// }
 
-const Meta = props => (
-  <>
-    <link rel="canonical" href={props.path || '//hipi.com'} />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content={props.title || 'watch hipi'} />
-    <meta property="og:image" content={props.image || ''} />
-    <meta property="og:url" content={props.path} />
-    <meta
-      property="og:description"
-      content="This project is a starter kit that can be used to build a robust and high performance web app."
-    />
+// const Meta = props => (
+//   <>
+//     <link rel="canonical" href={props.path || '//hipi.com'} />
+//     <meta property="og:type" content="article" />
+//     <meta property="og:title" content={props.title || 'watch hipi'} />
+//     <meta property="og:image" content={props.image || ''} />
+//     <meta property="og:url" content={props.path} />
+//     <meta
+//       property="og:description"
+//       content="This project is a starter kit that can be used to build a robust and high performance web app."
+//     />
 
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content="hipi Web Starter Kit" />
-    {/* twitter needs absolute urls for image */}
-    {/* <meta name="twitter:image" content="https://covid.bhaarat.ai/img_fb.jpg" /> */}
-    <meta name="twitter:image:alt" content="hipi Web Starter Kit" />
+//     <meta name="twitter:card" content="summary" />
+//     <meta name="twitter:title" content="hipi Web Starter Kit" />
+//     {/* twitter needs absolute urls for image */}
+//     {/* <meta name="twitter:image" content="https://covid.bhaarat.ai/img_fb.jpg" /> */}
+//     <meta name="twitter:image:alt" content="hipi Web Starter Kit" />
 
-    <meta
-      name="description"
-      itemProp="description"
-      content="This project is a starter kit that can be used to build a robust and high performance web app"
-    />
-    <meta name="keywords" content="hipi web starter kit" />
-    <meta name="format-detection" content="telephone=no" />
-  </>
-);
+//     <meta
+//       name="description"
+//       itemProp="description"
+//       content="This project is a starter kit that can be used to build a robust and high performance web app"
+//     />
+//     <meta name="keywords" content="hipi web starter kit" />
+//     <meta name="format-detection" content="telephone=no" />
+//   </>
+// );
 
-const SeoMeta = React.memo(Meta, isNewPage);
+// const SeoMeta = React.memo(Meta, isNewPage);
+
+const SeoMeta = () => (<NextSeo config={DEFAULT_SEO} />);
 
 export default SeoMeta;
