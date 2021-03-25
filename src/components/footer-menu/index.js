9716a9ed537cae68bb93bf25591299c5a0242b1d
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-03-25 08:41:49 +0530

    change4

--- src/components/footer-menu/index.js
+++ src/components/footer-menu/index.js
@@ -8,17 +8,15 @@ import useTranslation from '../../hooks/use-translation';
 
 function FooterMenu() {
 
-  const vobj = {'videoId' : '{cfcb3a92-50bb-4b37-a51d-b142ef3cc3be}' };
-  const { show } = useDrawer();
+  const vobj = {videoId : "cfcb3a92-50bb-4b37-a51d-b142ef3cc3be" };
+  // const { show } = useDrawer();
   const { t } = useTranslation();
   return (
     <div className="w-full bg-black fixed bottom-0 py-2 flex justify-around items-center">
       <Home />
       <Search />
       <button
-        className="rounded-full text-white py-0.5 px-4 bg-hipipink font-medium tracking-wide xxs:text-sm xs:text-base"
-        
-        onClick={() => cbplugin && cbplugin.cbTouch(vobj)}
+        className="rounded-full text-white py-0.5 px-4 bg-hipipink font-medium tracking-wide xxs:text-sm xs:text-base" onClick={() => cbplugin && cbplugin.cbTouch(vobj)}
       >
         {t('shop')}
       </button>
