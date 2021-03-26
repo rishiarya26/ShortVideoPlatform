57bdf46ad041db69afb389d5ccaa94ccce5b8501
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-03-26 15:05:34 +0530

    update to alpha

--- src/components/footer-menu/index.js
+++ src/components/footer-menu/index.js
@@ -2,7 +2,7 @@ import Home from '../commons/svgicons/home';
 import Add from '../commons/svgicons/add';
 import Search from '../commons/svgicons/search';
 import Profile from '../commons/svgicons/profile';
-import useDrawer from '../../hooks/use-drawer';
+// import useDrawer from '../../hooks/use-drawer';
 import useTranslation from '../../hooks/use-translation';
 // import ShoppingWidget from '../shopping-widget';
 
@@ -16,14 +16,9 @@ function FooterMenu() {
       <Home />
       <Search />
       <button
-<<<<<<< HEAD
         className="rounded-full text-white py-0.5 px-4 bg-hipipink font-medium tracking-wide xxs:text-sm xs:text-base" onClick={() => cbplugin && cbplugin.cbTouch(vobj)}
-=======
-        className="rounded-full text-white py-1 px-4 bg-hipipink font-medium tracking-wide xxs:text-sm xs:text-base"
-        onClick={() => show('', ShoppingWidget)}
->>>>>>> 94d1df9db19bc25c451e22fbc7123ec3eaff9179
       >
-        {t('shop')}
+        SHOP
       </button>
       <div id="cb_tg_d_wrapper">
         <div className="playkit-player">
