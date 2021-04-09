c06b693972ad2ed966b689f4c8ce0ea2629e8e0e
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-04-09 12:13:15 +0530

    seekbar pulled

--- src/components/seekbar/index.js
+++ src/components/seekbar/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Seekbar(props) {
   return (
     <div className="w-full h-1 fixed bottom-11">
-      <div className="relative w-400 h-1 overflow-hidden z-10 bg-black">
+      <div className="relative w-400 h-2  bg-black">
         <span style={{ width: `${props.seekedPercentage}%` }} className="absolute top-0 left-0 h-1 bg-gray-100 " />
       </div>
     </div>
