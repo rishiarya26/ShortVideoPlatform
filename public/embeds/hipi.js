954b92eb68b8d9ea48e73e84fa296ce8d531e35d
Ankit Agarwal <ankit.agarwal@ZL-MU-MT-MAC100.local>
2021-04-23 15:48:14 +0530

    change the url

--- public/embeds/hipi.js
+++ public/embeds/hipi.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 function prepareFrame() {
   var ifrm = document.createElement('iframe');
-  ifrm.setAttribute('src', 'https://hipi-web.s3.ap-south-1.amazonaws.com/embed.html');
+  ifrm.setAttribute('src', 'https://hipi.co.in/embed/cbvtest1mq99gi6b');
   ifrm.setAttribute('style', 'height: 100%; width: 100%; top: 0; left: 0; position: absolute;');
   ifrm.setAttribute('loading', 'lazy');
   ifrm.setAttribute('title', 'hipi');
@@ -23,4 +23,3 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 {/* <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@kendogkenny/video/6939524081066773766" data-video-id="6939524081066773766" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@kendogkenny" href="https://www.tiktok.com/@kendogkenny">@kendogkenny</a> <p>Reply to @yvonnec1618  PLEASE— these comments are too funny I don’t even like alcohol that much 😭✊🏼</p> <a target="_blank" title="♬ Circus - Music and Crowd Sound Effect - Hollywood Sound Effects" href="https://www.tiktok.com/music/Circus-Music-and-Crowd-Sound-Effect-6758350390116222977">♬ Circus - Music and Crowd Sound Effect - Hollywood Sound Effects</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script> */}
 
-// ifrm.setAttribute('src', 'https://hipi-web.s3.ap-south-1.amazonaws.com/embed.html');
