fd03f918c0bdbbd06b1fa2ffef6159b804427e67
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-05-03 19:07:38 +0530

    download drawer done

--- src/components/detect-operating-system-modal/index.js
+++ src/components/detect-operating-system-modal/index.js
@@ -6,12 +6,14 @@ export default function detectOperatingSystemModal({ handleOperatingsystem }) {
   return (
     <>
       <div className=" flex items-center justify-center flex-col bg-gray-900 w-full ">
-      <Logo />
-      <p className="text-white text-lg my-2" >The best experience is in App</p>
+        <Logo />
+        <p className="text-white text-lg my-2">The best experience is in App</p>
         {/* {handleOperatingsystem()} */}
-       { handleOperatingsystem() === 'android' ? 
-        <img src={withBasePath('icons/play_store.png')} className="" alt="playicon" /> : <img src={withBasePath('icons/app_store.png')} className="" alt="playicon" />
-   } </div>
+        { handleOperatingsystem() === 'android'
+          ? <img src={withBasePath('icons/play_store.png')} className="" alt="playicon" /> : <img src={withBasePath('icons/app_store.png')} className="" alt="playicon" />}
+        {' '}
+
+      </div>
     </>
-  )
+  );
 }
