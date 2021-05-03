5cf505e2a29068d753d7261ee8e11c7bd6d80acd
Rishi Arya <rishi.arya@zee.com>
2021-05-03 19:33:11 +0530

    web added for condition

--- src/components/detect-operating-system-modal/index.js
+++ src/components/detect-operating-system-modal/index.js
@@ -8,11 +8,14 @@ export default function detectOperatingSystemModal({ handleOperatingsystem }) {
       <div className=" flex items-center justify-center flex-col bg-gray-900 w-full ">
         <Logo />
         <p className="text-white text-lg my-2">The best experience is in App</p>
-        {/* {handleOperatingsystem()} */}
-        { handleOperatingsystem() === 'android'
-          ? <img src={withBasePath('icons/play_store.png')} className="" alt="playicon" /> : <img src={withBasePath('icons/app_store.png')} className="" alt="playicon" />}
+        { handleOperatingsystem() === 'Android'
+          ? <img src={withBasePath('icons/play_store.png')} className="" alt="playicon" /> : 
+          handleOperatingsystem() === 'iOS' 
+          ? <img src={withBasePath('icons/app_store.png')} className="" alt="playicon" /> :
+          <span className="text-white">
+            WEB
+         </span>}
         {' '}
-
       </div>
     </>
   );
