adecd0dda60b645ca1dc025216b14a0e9824566a
Rishi Arya <rishi.arya@zee.com>
2021-05-20 22:41:24 +0530

    updated sample-embed id, some design

--- src/components/video-card/loader.js
+++ src/components/video-card/loader.js
@@ -1,7 +1,7 @@
 function VideoCard() {
   return (
     <div className="">
-      <div className="p-0.5 rounded-md m-px bg-gray-400 h-28 w-28" />
+      <div className="p-0.5 rounded-sm m-px bg-gray-400 h-40 w-28" />
     </div>
 
   );
