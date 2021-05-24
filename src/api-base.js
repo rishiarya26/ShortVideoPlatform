bd519995617b248efae3bc70413652ee8329a5a1
Ankit Agarwal <ankit.agarwal@ZL-MU-MT-MAC100.local>
2021-05-24 17:24:13 +0530

    enable mock mode for server side calls

--- src/api-base.js
+++ src/api-base.js
@@ -4,7 +4,7 @@ export const apiBaseEndPoints = {
   local: {
     test: 'https://api.github.com',
     hipi: 'https://hipigwapi.zee5.com/api',
-    translations: 'http://localhost:3000',
+    app: 'http://localhost:3000',
     stagingMobile: 'https://stagingmobile.charmboard.com',
     'get-social': 'https://api.getsocial.im/v1',
     login: 'https://whapi.zee5.com/v1/user'
@@ -12,7 +12,7 @@ export const apiBaseEndPoints = {
   development: {
     test: 'https://api.github.com',
     hipi: 'https://hipigwapi.zee5.com/api',
-    translations: 'https://mock.hipi.co.in',
+    app: 'https://mock.hipi.co.in',
     stagingMobile: 'https://stagingmobile.charmboard.com',
     'get-social': 'https://api.getsocial.im/v1',
     login: 'https://whapi.zee5.com/v1/user'
@@ -20,7 +20,7 @@ export const apiBaseEndPoints = {
   staging: {
     test: 'https://api.github.com',
     hipi: 'http://3.6.36.112:7000/api',
-    translations: 'https://preprod.hipi.co.in',
+    app: 'https://preprod.hipi.co.in',
     'get-social': 'https://api.getsocial.im/v1',
     stagingMobile: 'https://stagingmobile.charmboard.com',
     login: 'https://whapi.zee5.com/v1/user'
@@ -28,7 +28,7 @@ export const apiBaseEndPoints = {
   production: {
     test: 'https://api.github.com',
     hipi: 'https://hipigwapi.zee5.com/api',
-    translations: 'https://www.hipi.co.in',
+    app: 'https://www.hipi.co.in',
     'get-social': 'https://api.getsocial.im/v1',
     stagingMobile: 'https://stagingmobile.charmboard.com',
     login: 'https://whapi.zee5.com/v1/user'
