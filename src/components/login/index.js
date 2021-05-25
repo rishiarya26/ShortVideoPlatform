7654bd47aa63111d03cbec66680dbcc420715a85
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-05-25 13:56:43 +0530

    login phase1

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,4 +1,4 @@
-import Fb from '../commons/svgicons/facebook/'
+import Fb from '../commons/svgicons/facebook';
 
 export default function Login() {
   return (
@@ -11,21 +11,21 @@ export default function Login() {
         <div className="flex border border-2 border-gray-300 py-2 px-4 w-full my-2">
           <div className="justify-self-start"><Fb /></div>
           <div className="flex justify-center w-full font-semibold">
-          <p>Use phone or email</p>
+            <p>Use phone or email</p>
           </div>
-        </div> 
+        </div>
         <div className="flex border border-2 border-gray-300 py-2 px-4 w-full my-2">
           <div className="justify-self-start">dd</div>
           <div className="flex justify-center w-full font-semibold">
-          <p>Continue with facebook</p>
+            <p>Continue with facebook</p>
           </div>
-        </div> 
+        </div>
         <div className="flex border border-2 border-gray-300 py-2 px-4 w-full my-2">
           <div className="justify-self-start">dd</div>
           <div className="flex justify-center w-full font-semibold">
-          <p>Continue with google</p>
+            <p>Continue with google</p>
           </div>
-        </div> 
+        </div>
       </div>
     </div>
   );
