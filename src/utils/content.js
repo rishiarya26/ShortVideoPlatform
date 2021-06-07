ff53f1611ba4306096cad5e839e3b9dafc57ce6e
Rishi Arya <rishi.arya@zee.com>
2021-06-07 15:16:33 +0000

    Merged in develop (pull request #111)
    Develop
    
    * Merged in sample-embed (pull request #108)
    
    embed domain to preprod
    
    * embed domain to preprod
    
    Approved-by: Rishi Arya
    
    * Merged in embed-url-copy (pull request #110)
    
    updated feed & shop flow. Other minor changes
    
    * updated feed & shop flow. Other minor changes
    
    * updated feed & shop flow. Other minor changes
    
    Approved-by: ankit.agarwal

--- src/utils/content.js
+++ src/utils/content.js
@@ -1,11 +1,11 @@
 import { getNetworkConnection } from './device-details';
 
 export const getEffectiveVideoUrl = urls => {
-  let videoUrl = urls.low;
+  let videoUrl = urls?.low;
   const networkConnection = getNetworkConnection();
-  if (networkConnection === 'fast' && urls.fast) {
+  if (networkConnection === 'fast' && urls?.fast) {
     videoUrl = urls.fast;
-  } else if (networkConnection === 'medium' && urls.medium) {
+  } else if (networkConnection === 'medium' && urls?.medium) {
     videoUrl = urls.medium;
   }
   return videoUrl;
