6e1aa045b2ec440d5194ccac75d5997a4ba86cdc
Rishi Arya <rishi.arya@zee.com>
2021-07-07 18:02:44 +0530

    .

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class Hipi extends Document {
   static async getInitialProps(ctx) {
     const userAgent = ctx?.req?.headers?.['user-agent'];
     const device = getDeviceType(userAgent);
-    ctx?.res?.setHeader('Set-Cookie',`deviceType=${device}; HttpOnly; MaxAge=${(365 * 24 * 60 * 60)}; Path=/`);
+    ctx?.res?.setHeader('Set-Cookie', `deviceType=${device}; HttpOnly; MaxAge=${(365 * 24 * 60 * 60)}; Path=/`);
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
