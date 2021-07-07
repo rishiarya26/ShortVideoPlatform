8c48864042eec590d90b30620880eec4a4c74924
Rishi Arya <rishi.arya@zee.com>
2021-07-07 17:46:08 +0530

    setting deviceType in resp cookies

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
 import Document, {
   Html, Main, NextScript, Head
 } from 'next/document';
+import { getDeviceType } from '../src/hooks/use-device';
 
 class Hipi extends Document {
   static async getInitialProps(ctx) {
+    const userAgent = ctx?.req?.headers?.['user-agent'];
+    const device = getDeviceType(userAgent);
+    ctx?.res?.setHeader('Set-Cookie',`deviceType=${device}; HttpOnly; MaxAge=${(365 * 24 * 60 * 60)}; Path=/`);
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
--- src/hooks/use-device.js
+++ src/hooks/use-device.js
@@ -2,18 +2,15 @@ import { useState } from 'react';
 
 export const devices = ['desktop', 'mobile'];
 
-export const getDeviceType = () => {
+export const getDeviceType = userAgent => {
   let device = 'desktop';
-  if (!navigator) {
-    return device;
-  }
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) device = 'mobile';
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) device = 'mobile';
   return device;
 };
 
 function useDevice(types = devices, values, defaultValue) {
   const getVariant = () => {
-    const device = getDeviceType();
+    const device = getDeviceType(navigator?.userAgent);
     const index = types.indexOf(device);
     return values[index] ? values[index] : defaultValue;
   };
