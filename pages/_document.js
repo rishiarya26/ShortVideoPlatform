a11e3f16f74512c01029cb8804145e9719f4cda0
Rishi Arya <rishi.arya@zee.com>
2021-07-22 12:49:05 +0530

    updtaed PR comments

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,8 @@ class Hipi extends Document {
   static async getInitialProps(ctx) {
     const userAgent = ctx?.req?.headers?.['user-agent'];
     const device = getDeviceType(userAgent);
-    const maxAge = (365 * 24 * 60 * 60)
+    const maxAge = (365 * 24 * 60 * 60);
     ctx?.res?.setHeader('Set-Cookie', `device-type=${device}; HttpOnly; MaxAge=${maxAge};`);
-    console.log("response-deasers",ctx.res)
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
