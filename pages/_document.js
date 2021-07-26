cf2c7e959c06d9877dc61f8fa97992235c9f8e19
Rishi Arya <rishi.arya@zee.com>
2021-07-26 06:10:46 +0000

    Merged in deploy-new (pull request #175)
    Deploy new
    
    * Merge pull request #19 from ZEE-5/detect-device-set-cookie
    
    Setting deviceType in resp cookies from server side.
    * .
    
    * fix issue
    
    * .
    
    * added semi coln
    
    * .
    
    * fix-issue
    
    * fix-issue
    
    * fix safari issue with embed (#26)
    
    * .
    
    * fix issue
    
    * .
    
    * added semi coln
    * .

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
 import Document, {
   Html, Main, NextScript, Head
 } from 'next/document';
+import { getDeviceType } from '../src/hooks/use-device';
 
 class Hipi extends Document {
   static async getInitialProps(ctx) {
+    const userAgent = ctx?.req?.headers?.['user-agent'];
+    const device = getDeviceType(userAgent);
+    const maxAge = (365 * 24 * 60 * 60);
+    ctx?.res?.setHeader('Set-Cookie', `device-type=${device}; HttpOnly; MaxAge=${maxAge};`);
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
--- src/hooks/use-device.js
+++ src/hooks/use-device.js
@@ -1,19 +1,13 @@
+/* eslint-disable max-len */
 import { useState } from 'react';
 
 export const devices = ['desktop', 'mobile'];
 
-export const getDeviceType = () => {
-  let device = 'desktop';
-  if (!navigator) {
-    return device;
-  }
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) device = 'mobile';
-  return device;
-};
+export const getDeviceType = userAgent => ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) ? 'mobile' : 'desktop');
 
 function useDevice(types = devices, values, defaultValue) {
   const getVariant = () => {
-    const device = getDeviceType();
+    const device = getDeviceType(navigator?.userAgent);
     const index = types.indexOf(device);
     return values[index] ? values[index] : defaultValue;
   };
