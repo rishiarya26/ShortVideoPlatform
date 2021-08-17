27c7e923387eaa7aec7cb57b4e1180254a4d534d
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-08-17 19:23:21 +0530

    updated UI

--- src/components/commons/svgicons/comment/index.js
+++ src/components/commons/svgicons/comment/index.js
@@ -1,53 +1,20 @@
 /* eslint-disable max-len */
 
 const Comment = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="40"
-    height="40"
-    fill="none"
-    viewBox="0 0 48 48"
-  >
+  <svg width="40" height="40" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g filter="url(#filter0_b)">
-      <rect
-        width="48"
-        height="48"
-        fill="#fff"
-        fillOpacity="0.2"
-        rx="24"
-      />
+    <rect width="48" height="48" rx="24" fill="black" fill-opacity="0.2"/>
     </g>
-    <path
-      fill="#fff"
-      d="M18.001 14a6 6 0 00-6 6v4c0 .24.014.477.042.71A1 1 0 0012 25v8.826a1 1 0 001.65.759l4.788-4.104A2 2 0 0119.74 30H30a6 6 0 006-6v-4a6 6 0 00-6-6h-12z"
-    />
+    <path d="M18.001 14C14.6873 14 12.001 16.6863 12.001 20V24C12.001 24.2403 12.0151 24.4774 12.0426 24.7104C12.0149 24.802 12 24.8993 12 25V33.8258C12 34.6801 13.0021 35.141 13.6508 34.585L18.4383 30.4815C18.8008 30.1708 19.2624 30 19.7398 30H30.001C33.3147 30 36.001 27.3137 36.001 24V20C36.001 16.6863 33.3147 14 30.001 14H18.001Z" fill="white"/>
     <defs>
-      <filter
-        id="filter0_b"
-        width="52"
-        height="52"
-        x="-2"
-        y="-2"
-        colorInterpolationFilters="sRGB"
-        filterUnits="userSpaceOnUse"
-      >
-        <feFlood floodOpacity="0" result="BackgroundImageFix" />
-        <feGaussianBlur
-          in="BackgroundImage"
-          stdDeviation="1"
-        />
-        <feComposite
-          in2="SourceAlpha"
-          operator="in"
-          result="effect1_backgroundBlur"
-        />
-        <feBlend
-          in="SourceGraphic"
-          in2="effect1_backgroundBlur"
-          result="shape"
-        />
-      </filter>
+    <filter id="filter0_b" x="-2" y="-2" width="52" height="52" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feGaussianBlur in="BackgroundImage" stdDeviation="1"/>
+    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape"/>
+    </filter>
     </defs>
-  </svg>
+</svg>
+
 );
 export default Comment;
