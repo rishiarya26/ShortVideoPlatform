3d597ad1a8e166e004a3e7b6345dbe8d6fcc4ca5
Cristbin Sanchez <cristbin.sanchez@charmboard.com>
2021-08-25 15:41:07 +0530

    hashtag and user listing done

--- pages/explore.js
+++ pages/explore.js
@@ -1,10 +1,15 @@
 import Explore from '../src/components/explore';
 import SearchResult from '../src/components/search/search-result'
+import ExploreUserList from '../src/components/explore-user-list'
+import HashTag from '../src/components/explore-hashtag'
 
 export default function Hipi() {
   return (
     <div>
-    <Explore />
+    {/* <Explore /> */}
+    {/* <SearchResult/> */}
+    {/* <ExploreUserList/> */}
+    <HashTag/>
     </div>
   );
 }
