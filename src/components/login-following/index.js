4a715e03d109dbaa8aebea637659d1da49576e32
Rishi Arya <rishi.arya@zee.com>
2021-09-07 21:58:39 +0530

    added loading page for follwing

--- src/components/login-following/index.js
+++ src/components/login-following/index.js
@@ -1,6 +1,8 @@
 /*eslint-disable react/display-name */
 import dynamic from "next/dynamic";
 import useDrawer from "../../hooks/use-drawer";
+import Door from "../commons/svgicons/door-open";
+import FooterMenu from "../footer-menu";
 
 const login = dynamic(
     () => import('../auth-options'),
@@ -18,24 +20,25 @@ const LoginFollowing = () =>{
       };
     
     return(
-    //     <div className="h-screen bg-black  w-screen flex flex-col items-center justify-center">
-    //     <div className="w-40 h-40 flex items-center justify-center bg-white rounded-full">
-    //       <Door/>
-    //     </div>
-    //     <div className="text-lg text-white font-bold mt-8">
-    //       Login to Follow Users
-    //     </div>
-    //     <div className="text-white mt-3">
-    //       Kindly log in to start following other users
-    //     </div>
-    //     <div className="bg-red-600 rounded px-12 py-2 flex justify-center items-center text-white mt-8">
-    //       Log In
-    //     </div>
-    //   </div>
-      <div className='w-screen h-screen bg-black'>
-          <button onClick={showLoginOptions} className='w-10 h-5 bg-white'>
+        <div className="h-screen bg-black  w-screen flex flex-col items-center justify-center">
+        <div className="w-32 h-32 flex items-center justify-center bg-white rounded-full">
+          <Door/>
+        </div>
+        <div className="text-lg text-white font-bold mt-8">
+          Login to Follow Users
+        </div>
+        <div className="text-white mt-3">
+          Kindly log in to start following other users
+        </div>
+        <div className="bg-red-600 rounded px-12 py-2 flex justify-center items-center text-white mt-8">
+        <button onClick={showLoginOptions} className='w-10 h-5 bg-white'>
               Login
           </button>
+        </div>
+        <FooterMenu
+          type="noshop"
+          selectedTab="home"
+        />
       </div>
     )
 }
