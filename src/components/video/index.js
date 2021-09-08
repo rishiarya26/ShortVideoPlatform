ce4fa2a15abc94333b7812772fabb5b909510e88
Rishi Arya <rishi.arya@zee.com>
2021-09-08 16:00:48 +0530

    autoplay changed

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -71,7 +71,7 @@ function Video(props) {
     percentage && props.updateSeekbar(percentage);
   };
 
-  props.initialPlayButton === false && rootRef.current.children[0].play()
+  // props.initialPlayButton === false && rootRef.current.children[0].play()
 
   // const unMute =(e)=>{
   //   rootRef.current.children[0].muted = false;
@@ -79,19 +79,19 @@ function Video(props) {
   //   props.setMuted(false);
   // }
 
-  let hasPlayed = false;
-  function handleFirstPlay(event) {
-  if(hasPlayed === false) {
-    hasPlayed = true;
+  // let hasPlayed = false;
+  // function handleFirstPlay(event) {
+  // if(hasPlayed === false) {
+  //   hasPlayed = true;
 
-    let vid = event.target;
+  //   let vid = event.target;
 
-    vid.onplay = null;
+  //   vid.onplay = null;
     // props.initialPlayButton === false && rootRef.current.children[0].play()
 
     // Start whatever you need to do after first playback has started
-  }
-}
+//   }
+// }
 
   return (
     <div
@@ -101,8 +101,8 @@ function Video(props) {
     >
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
       <video
-        // autoPlay
-        // muted
+        autoPlay
+        muted
         // muted={props.muted ? true : false}
         webkit-playsinline = "true"
         // onLoadCapture ={resetCurrentTime}
