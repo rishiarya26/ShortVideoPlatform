f5e73559219799ee965b4846a680e67c3bc956fc
Rishi Arya <rishi.arya@zee.com>
2021-09-16 16:17:41 +0000

    Merged in add-shop (pull request #249)
    Add shop
    
    * .
    
    * loader#fix
    
    * Merge branch 'add-shop' of https://bitbucket.org/zee5in/hipi-web into add-shop
    
    * fix Landscape
    
    * .
    
    * .
    
    * landscape Fix
    
    * feed loader fix
    
    * feed loader fix
    
    * feed loader fix

---
+++ src/components/landscape/index.js
@@ -0,0 +1,13 @@
+/*eslint-disable @next/next/no-img-element */
+import { withBasePath } from '../../config';
+
+function Landscape() {
+  return (
+    <div className=" landscape_screen w-full h-full flex justify-center items-center z-90 bg-gray-400 bg-opacity-70 absolute top-0 left-0 overflow-hidden">
+      <img className="w-16" src={withBasePath('images/mobile-switch-white.png')} alt="hipi logo" /> 
+      <p className="text-white text-2xl">Please rotate your device</p>
+    </div>
+  );
+  }
+export default Landscape;
+
