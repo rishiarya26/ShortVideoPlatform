a719dd8bdee5d5bc19be0adfb0544940b9c8ea5d
Rishi Arya <rishi.arya@zee.com>
2021-09-16 10:45:03 +0530

    .

--- src/utils/app.js
+++ src/utils/app.js
@@ -41,11 +41,17 @@ export const generateUUID = persist => {
 };
 
 export const share = (id) => {
+  // const url = document?.location?.href;
+  // let domain = (new URL(url));
+  // domain = domain?.hostname;
+  // const finalUrl = (id && domain && `https://${domain}/video${id}`) || document?.location?.href;
+  // console.log(`https://${domain}/video/${id}`)
+  // console.log(finalUrl)
   if (navigator.share) {
    try{ const url = document?.location?.href;
     let domain = (new URL(url));
     domain = domain?.hostname;
-    const finalUrl = (id && domain && `https://${domain}/video${id}`) || document?.location?.href;
+    const finalUrl = (id && domain && `https://${domain}/video/${id}`) || document?.location?.href;
     // const canonicalElement = document.querySelector('link[rel=canonical]');
     // const url = canonicalElement?.href || document.location.href;
     return navigator.share({
