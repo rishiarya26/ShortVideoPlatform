2a8d08a9b01285d63636b6f0e1b9bd04908d6535
Rishi Arya <rishi.arya@zee.com>
2021-09-23 09:24:16 +0000

    Merged in add-shop (pull request #269)
    Add shop
    
    * added consoles fo embed
    
    * .
    
    * .
    
    * .
    
    * .
    
    * replaced embed download links
    
    * .
    
    * .
    
    * changes for embed
    
    * changes for embed

--- src/components/embed-video-sidebar/index.js
+++ src/components/embed-video-sidebar/index.js
@@ -7,19 +7,38 @@ import Follow from '../commons/svgicons/follow';
 import Comment from '../commons/svgicons/comment';
 import Share from '../commons/svgicons/share';
 import useDrawer from '../../hooks/use-drawer';
+import { getOS } from '../../utils/device-details';
 
-const detectDeviceModal = dynamic(
-  //() => import('../download-app-widget'),
-  () => import('../open-in-app'),
-  {
-    loading: () => <div />,
-    ssr: false
-  }
-);
+// const detectDeviceModal = dynamic(
+//   //() => import('../download-app-widget'),
+//   () => import('../open-in-app'),
+//   {
+//     loading: () => <div />,
+//     ssr: false
+//   }
+// );
 
 
 function EmbedVideoSidebar(props) {
-  const { show } = useDrawer();
+  // const { show } = useDrawer();
+
+  const stores = {
+    android: 'https://play.google.com/store/apps/details?id=com.zee5.hipi',
+    ios: 'https://apps.apple.com/in/app/zee5-shows-live-tv-movies/id743691886'
+  };
+
+  const onStoreRedirect =()=>{
+    console.log('clicked')
+    let deviceInfo = 'android';
+   try{ 
+     deviceInfo = getOS();
+     deviceInfo && (window.open(`${stores[deviceInfo]}`));
+     console.log('clicked','window',window.open,'e',getOS(),'f',`${stores[deviceInfo]}`)
+   }
+    catch(e){
+      console.log('error in store redirect')
+      return `${stores[deviceInfo]}`}
+    }
 
   const info = {
     single : 'bottom-28 fixed',
@@ -29,7 +48,7 @@ function EmbedVideoSidebar(props) {
   return (
     <div className={`right-0 text-white ${info[props.type]}`}>
       <div
-        onClick={() => show('', detectDeviceModal, 'extraSmall',{text: "see profile"})}
+        onClick={onStoreRedirect}
         className="relative py-2 px-3 text-center flex justify-center"
       >
         <img
@@ -38,32 +57,31 @@ function EmbedVideoSidebar(props) {
           src={props.profilePic || "https://akamaividz2.zee5.com/image/upload/w_297,c_scale,f_auto,q_auto/v1625388234/hipi/videos/c3d292e4-2932-4f7f-ad09-b974207b1bbe/c3d292e4-2932-4f7f-ad09-b974207b1bbe_00.webp"}
         />
         <div
-          onClick={() => show('', detectDeviceModal, 'extraSmall',{text: "follow"})}
+          onClick={onStoreRedirect}
           className="absolute bottom-0 left-1/3"
         >
           <Follow />
         </div>
       </div>
       <div
-        onClick={() => show('', detectDeviceModal, 'extraSmall',{text: "like"})}
+        onClick={onStoreRedirect}
         className="relative py-2 px-3 text-center flex flex-col items-center"
       >
         <Like />
         <p className="text-sm">{props.likes}</p>
       </div>
       <div
-        onClick={() => show('', detectDeviceModal, 'extraSmall',{text: "comment"})}
+        onClick={onStoreRedirect}
         className="relative py-2 px-3 text-center flex flex-col items-center"
       >
         <Comment />
         {/* <p className="text-sm">{props.likes}</p> */}
       </div>
       <div
-        onClick={() => show('', detectDeviceModal, 'extraSmall',{text: "share"})}
+        onClick={onStoreRedirect}
         className="relative py-2 px-3 text-center flex flex-col items-center"
       >
         <Share />
-        
         <p className="text-sm text-center">Share</p>
       </div>
     </div>
--- src/components/embed-video/index.js
+++ src/components/embed-video/index.js
@@ -26,7 +26,7 @@ function Embedvideo(props) {
    try{ 
      deviceInfo = getOS();
      deviceInfo && (window.open(`${stores[deviceInfo]}`));
-     console.log('clicked','window',window.open,'e',deviceInfo,'f',`${stores[deviceInfo]}`)
+     console.log('clicked','window',window.open,'e',getOS(),'f',`${stores[deviceInfo]}`)
    }
     catch(e){
       console.log('error in store redirect')
@@ -138,7 +138,7 @@ function Embedvideo(props) {
         comp="embed"
       />
       <div className="flex w-full py-4 justify-center border-t-2 border-gray-200 mt-2">
-            <button onClick={onStoreRedirect} className="bg-hipired text-white px-4 py-2 font-semibold">View more on Hipi</button>
+            <button onClick={onStoreRedirect} className="bg-hipired text-white px-4 py-2 font-semibold  text-sm">View more on Hipi</button>
       </div>
     </div>
     </div>
