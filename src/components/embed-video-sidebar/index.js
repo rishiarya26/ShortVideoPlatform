9bd6429b8b3aea5fe8fa2af3b4848c6905603a84
Rishi Arya <rishi.arya@zee.com>
2021-09-23 19:48:01 +0530

    updated ios link

--- src/components/embed-video-sidebar/index.js
+++ src/components/embed-video-sidebar/index.js
@@ -24,7 +24,7 @@ function EmbedVideoSidebar(props) {
 
   const stores = {
     android: 'https://play.google.com/store/apps/details?id=com.zee5.hipi',
-    ios: 'https://apps.apple.com/in/app/zee5-shows-live-tv-movies/id743691886'
+    ios: 'https://hipi.onelink.me/tMco/1e3e4b5e'
   };
 
   const onStoreRedirect =()=>{
