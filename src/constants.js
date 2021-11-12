78335c3f001df9e08aca25d3d4d5a1119fd42024
Rishi Arya <rishi.arya@zee.com>
2021-11-12 17:39:44 +0530

    removed robot.txt

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const MIXPANEL_PROD = '28ad1bd3f9fbcda7356c70835a0b4d96';
 export const ONE_TAP_DOWNLOAD = 'https://hipi.onelink.me/tMco/1e3e4b5e';
 export const ANDROID_STORE = 'https://play.google.com/store/apps/details?id=com.zee5.hipi';
 export const IOS_STORE = 'https://apps.apple.com/in/app/hipi-indian-short-video-app/id1561950008';
-export const VERSION = '1.2.2';
+export const VERSION = '1.3.0';
 export const APP_NAME = 'HiPi';
 export const LANGUAGE = 'en';
 export const GOOGLE_CLIENT_ID_PREROD = '1026747734321-0fobt02rbhi5j36kk6ft8el2k0tev9af.apps.googleusercontent.com';
