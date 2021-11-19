e1bcd3da6dfd3e24a45b9c9857e30a1620dfc973
Rishi Arya <rishi.arya@zee.com>
2021-11-19 15:03:42 +0000

    Merged in goat_production (pull request #363)
    Goat production
    
    * Goat production ver
    
    * .
    
    * .
    
    * added device type toggle
    
    * white band removed
    
    * white band removed
    
    * added fix
    
    * .
    
    * .
    
    * .

--- pages/goat.js
+++ pages/goat.js
@@ -1,18 +1,9 @@
-import GoatDesk from '../src/components/goat-desk';
-import GoatMob from '../src/components/goat-mob';
-import { getItem } from '../src/utils/cookie';
+import ChooseGoat from "../src/components/choose-goat";
 
 export default function Hipi() {
- const device = getItem('device-type') || 'desktop';
- console.log(device);
-
-  const selectedComp = {
-    'desktop' : <GoatDesk />,
-    'mobile' : <GoatMob/>
-  }
   return (
-    <>
-    {selectedComp[device]}
+  <>
+  <ChooseGoat/>
     </>
   );
 }
