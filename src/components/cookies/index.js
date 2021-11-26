aa652b22affc711694f023e6f1c12c5d374c2908
Rishi Arya <rishi.arya@zee.com>
2021-11-26 06:58:55 +0000

    Merged in country-detect (pull request #375)
    Country detect
    
    * added country detect logic
    
    * cookies cta added
    
    * added updates
    
    * .
    
    * .
    
    * .
    
    * added https api for country tracking
    
    * added https api for country tracking
    
    * added privacy policy
    
    * added privacy policy

--- src/components/cookies/index.js
+++ src/components/cookies/index.js
@@ -18,7 +18,7 @@ return (
       <p className="text-base pt-2">Our website uses cookies to enable basic functionality, analyze visitor traffic, provide social media features and deliver a better user experience.</p>
       <p className="text-base pt-2">We may also use cookies to help us deliver targeted content and assess the performance of that content and associated campaigns.</p>
       <p className="text-base pt-2">You may view or change your cookie preferences at any time via the cookie management link found within the footer. For more information, please review our</p>
-      <button onClick={()=>router && router.push('privacy-policy.html')} className="underline text-purple-600 text-sm text-left pt-2">Web Privacy Policy</button>
+      <button onClick={()=>router && router.push('/privacy-policy.html')} className="underline text-purple-600 text-sm text-left pt-2">Web Privacy Policy</button>
    </div>
    <div className="button_cook bg-gray-100 w-full p-4 py-8 flex flex-col items-center">
       <button onClick={cookiesAccepted} className="border-2 border-gray-500 p-2 px-6 rounded-full text-base cursor-pointer">Accept all cookies</button>
