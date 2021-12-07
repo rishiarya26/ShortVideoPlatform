3f705ce52bf6b009738a78389861e5c8124d8a64
Cristbin Sanchez <cristbin.sanchez@zee.com>
2021-12-07 04:31:40 +0000

    Merged in Goat_update_3 (pull request #391)
    Goat update 3
    
    * image added in goat
    
    * image added in goat
    
    * image replace
    
    * image replace
    
    * image replace
    
    * Merge branch 'production' of https://bitbucket.org/zee5in/hipi-web into Goat_update_3
    
    * Goat image upload
    
    * Goat image upload
    
    
    Approved-by: Rishi Arya

--- src/components/goat-mob/index.js
+++ src/components/goat-mob/index.js
@@ -73,7 +73,6 @@ const onStoreRedirect =(device)=>{
   <img src={withBasePath('images/goat/mobile/judge.jpg')}/>
   <img src={withBasePath('images/goat/mobile/10.jpg')}/>
   <img src={withBasePath('images/goat/mobile/11.jpg')}/>
-  <img src={withBasePath('images/goat/mobile/12.jpg')}/>
   </div>
   </>
 <div className="flex flex-col p-8 pt-0 px-4">
