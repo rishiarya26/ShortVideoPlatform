ccb7f58c8569eff93eb1b0dfbbaf359c713191f9
Rishi Arya <rishi.arya@zee.com>
2022-03-03 05:25:02 +0000

    Merged in android-style-iframe (pull request #533)
    Android style iframe
    
    * finalfixes
    
    * Merge branch 'android-style-iframe' of https://bitbucket.org/zee5in/hipi-web into android-style-iframe
    
    * added origin handling
    
    * .
    
    * FIXES#5
    
    * FIXES#5
    
    * FIXES#5
    
    * Merge branch 'production' of https://bitbucket.org/zee5in/hipi-web into android-style-iframe
    
    * Gray-hide
    
    * FIXES#5

--- src/components/charmboard/preview/index.js
+++ src/components/charmboard/preview/index.js
@@ -253,7 +253,7 @@ const CharmPreview = ({charmId, initalExpand = true, charms, loader, savedItems 
             </div>
             
          </div>
-         {expand && <div className='sticky w-full -top-4 pt-2 bg-gray-100 z-20 text-sm text-gray-200 border-gray-100 border-b-2'> <Tabs items={tabItems} onTabChange={onTabChange} selectedIndex={selectedIndex}/> </div>}
+         {expand && <div className='sticky w-full -top-4 pt-2 bg-grey-100 z-20 text-sm text-gray-200 border-grey-100 border-b-2'> <Tabs items={tabItems} onTabChange={onTabChange} selectedIndex={selectedIndex}/> </div>}
          {expand && 
          <div className="w-full" ref={outfitRef}>
          <div ref={outfit} id='outfit'>
