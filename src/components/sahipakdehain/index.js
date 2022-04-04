99b0b01254cdb7ac1c6e232dd784701f830b8994
Cristbin Sanchez MS <cristbin.sanchez@ZL-BLR-MAC408.local>
2022-04-04 21:28:44 +0530

    stage1#

---
+++ src/components/sahipakdehain/index.js
@@ -0,0 +1,58 @@
+/*eslint-disable @next/next/no-img-element */
+/*eslint-disable react/jsx-no-duplicate-props */
+
+import { withBasePath } from '../../config';
+
+function Sahipakdehain() {
+ 
+  return (
+    <div className="w-full h-full relative">
+       <div className='w-full '>
+       <img alt="" src={withBasePath('images/goat/mobile/1_1.jpg')} />
+       </div>
+       <div className='section py-4 px-6'>
+          <h2 className='text-base font-bold' >What is the contest?</h2>
+          
+          <p className='text-gray-500 text-sm mt-2'>We bring to you, the most iconic dialogues of one of the most favourite shows on Indian TV, Bhabiji Ghar Par Hai, which is Sahi Pakde Hain. </p>
+          <p className='text-gray-500 text-sm mt-2'> If you are a creator who dares to act in a fun way or loves to lipsync the dialogues & jokes and secretly wish that you could be that TV character, then this is for you.</p>
+          <p className='text-gray-500 text-sm mt-2'>A contest to ignite your passion of acting by becoming the Bhabiji with your fun take on it. A contest which offers you a platform where you allow your unique personality to show through these videos. </p>
+          <p className='text-gray-500 text-sm mt-2'>So wear your acting shoes and join us on this fun roller coaster ride by making fun videos from our pool of Sahi Pakde Hain dialogues. </p>
+       </div>
+
+       <div className='section py-4 px-6'>
+          <h2 className='text-base font-bold' >Who can participate?</h2>
+          
+          <p className='text-gray-500 text-sm mt-2'> If you love to act, if you love to create fun videos on social media, so this is for you.</p>
+          <p className='text-gray-500 text-sm mt-2'>Show us what you Got? </p>
+          
+       </div>
+
+       <div className='section py-4 px-6'>
+          <h2 className='text-base font-bold' >What is the contest?</h2>
+          
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+       </div>
+
+       <div className='section py-4 px-6'>
+          <h2 className='text-base font-bold' >What is the contest?</h2>
+          
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+       </div>
+
+       <div className='section py-4 px-6'>
+          <h2 className='text-base font-bold' >What is the contest?</h2>
+          
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> </p>
+       </div>
+    </div>
+  );
+}
+
+export default Sahipakdehain;
+
