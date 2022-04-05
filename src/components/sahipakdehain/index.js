95932f95d6dc48474820379e954279536806d6c2
Cristbin Sanchez MS <cristbin.sanchez@ZL-BLR-MAC408.local>
2022-04-05 09:33:05 +0530

    sph update

--- src/components/sahipakdehain/index.js
+++ src/components/sahipakdehain/index.js
@@ -6,11 +6,11 @@ import { withBasePath } from '../../config';
 function Sahipakdehain() {
  
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative pb-6">
        <div className='w-full '>
-       <img alt="" src={withBasePath('images/goat/mobile/1_1.jpg')} />
+       <img alt="" src={withBasePath('images/sph/SPH_Banner.jpg')} />
        </div>
-       <div className='section py-4 px-6'>
+       <div className='section pt-6 px-6'>
           <h2 className='text-base font-bold' >What is the contest?</h2>
           
           <p className='text-gray-500 text-sm mt-2'>We bring to you, the most iconic dialogues of one of the most favourite shows on Indian TV, Bhabiji Ghar Par Hai, which is Sahi Pakde Hain. </p>
@@ -19,37 +19,133 @@ function Sahipakdehain() {
           <p className='text-gray-500 text-sm mt-2'>So wear your acting shoes and join us on this fun roller coaster ride by making fun videos from our pool of Sahi Pakde Hain dialogues. </p>
        </div>
 
-       <div className='section py-4 px-6'>
+       <div className='section pt-4 px-6'>
           <h2 className='text-base font-bold' >Who can participate?</h2>
           
           <p className='text-gray-500 text-sm mt-2'> If you love to act, if you love to create fun videos on social media, so this is for you.</p>
           <p className='text-gray-500 text-sm mt-2'>Show us what you Got? </p>
           
        </div>
-
-       <div className='section py-4 px-6'>
-          <h2 className='text-base font-bold' >What is the contest?</h2>
-          
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' > What can you win?</h2>
+          <p className='text-gray-500 text-sm mt-2'>A chance to meet & greet your favorite Angoori Bhabiji on the sets of Bhabiji Ghar Par Hain. </p>
        </div>
 
-       <div className='section py-4 px-6'>
-          <h2 className='text-base font-bold' >What is the contest?</h2>
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >How to participate?</h2>
           
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'> <span className='text-black font-bold'>Step 1: </span>Download Hipi App from Play Store or App Store</p>
+          <p className='text-gray-500 text-sm mt-2'><span className='text-black font-bold'>Step 2: </span> Login or Signup on Hipi </p>
+          <p className='text-gray-500 text-sm mt-2'> <span className='text-black font-bold'>Step 3: </span> Click on the banner for #SahiPakdeHain on Discover screen, select your favorite dialogue, create & post your video (max 90secs). Don’t forget to the hashtag #SahiPakdeHain in the captions for the video.
+OR
+Click on the banner for #SahiPakdeHain on Discover screen, select your favorite video, create your duet video & post it (max 90secs). Don’t forget to the hashtag #SahiPakdeHain in the captions for the video.
+</p>
        </div>
 
-       <div className='section py-4 px-6'>
-          <h2 className='text-base font-bold' >What is the contest?</h2>
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >Rules and Regulations:</h2>
           
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
-          <p className='text-gray-500 text-sm mt-2'> </p>
+          <p className='text-gray-500 text-sm mt-2'>1. The videos or entries are only valid if the videos are uploaded with hashtag #SahiPakdeHain.</p>
+          <p className='text-gray-500 text-sm mt-2'>2. The video uploaded should be either a lipsync video or a duet video </p>
+          <p className='text-gray-500 text-sm mt-2'>3. The video duration should not exceed more than 90secs.</p>
+          <p className='text-gray-500 text-sm mt-2'>3. The video duration should not exceed more than 90secs. </p>
+          <p className='text-gray-500 text-sm mt-2'>4. To participate one needs to be 18+ years of age.  </p>
+          <p className='text-gray-500 text-sm mt-2'>5. Videos are allowed in hindi language only. </p>
+          <p className='text-gray-500 text-sm mt-2'>6. The video should be recorded in vertical format (16:9) </p>
+          <p className='text-gray-500 text-sm mt-2'>7. The creators can dress like the characters (optional) </p>
        </div>
+
+<div className='text-lg font-bold pt-4 px-6' >FAQ</div>
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >1.	Who can participate?</h2>
+          <p className='text-gray-500 text-sm mt-2'> Indian citizen above 18yrs</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >2.	How to make account on Hipi App?</h2>
+          <p className='text-gray-500 text-sm mt-2'> Download the App, register and login to your account and make videos</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >3.	Do you need to be a trained actor to participate?</h2>
+          <p className='text-gray-500 text-sm mt-2'> No, not at all. If you enjoy acting, you can participate</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >4.	Do I need to pay to download Hipi?</h2>
+          <p className='text-gray-500 text-sm mt-2'>No, its free & easy to use </p>
+       </div>
+       
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >5.	Do I need to pay to participate in the contest?</h2>
+          <p className='text-gray-500 text-sm mt-2'> No, it’s free for all to participate. Just download Hipi, create an account or login, and upload videos</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >6.	For how long is the contest?</h2>
+          <p className='text-gray-500 text-sm mt-2'> It’s an ongoing contest. Enjoy making videos on as many dialogues as much possible.</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >7.	What do I win?</h2>
+          <p className='text-gray-500 text-sm mt-2'>Every month the winner who gets highest engagement for his video, gets a chance to meet & greet your favorite Angoori Bhabiji on the sets of Bhabiji Ghar Par Hain. Engagement for video is total of likes, comments, shares, downloads per video view. </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >8.	Will my acting videos come on TV?</h2>
+          <p className='text-gray-500 text-sm mt-2'> No, your acting videos of contest will not come on TV</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >9.	Will I be paid to upload my acting videos?</h2>
+          <p className='text-gray-500 text-sm mt-2'>No, you will not be paid just for uploading videos.   </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >10.	Can I submit videos in any language?</h2>
+          <p className='text-gray-500 text-sm mt-2'> No, the videos should be in Hindi</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >11.	How many videos can I upload in 01 day?</h2>
+          <p className='text-gray-500 text-sm mt-2'>As many as you want on as many dialogues as you want. </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >12.	What is the duration of videos to be submitted?</h2>
+          <p className='text-gray-500 text-sm mt-2'>Not more than 90secs </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >13.	Do I need to submit solo videos, or Can I upload duet or group videos too?</h2>
+          <p className='text-gray-500 text-sm mt-2'>You can upload solo or duet videos, can have your friends or family members acting in it. Always use the relevant hashtag #SahiPakdeHain</p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >14.	What is engagement rate?</h2>
+          <p className='text-gray-500 text-sm mt-2'>Engagement rate for video is total of likes, comments, shares, downloads per video view. </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >15.	How many videos do I need to upload to be eligible in the contest?</h2>
+          <p className='text-gray-500 text-sm mt-2'>You need to upload minimum 01 video to be eligible for the contest using the relevant hashtag.  </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >16.	Do we need to upload everyday?</h2>
+          <p className='text-gray-500 text-sm mt-2'>There is no compulsion as such. But more videos, increases the chances of engagement & chance to meet & greet your favourite Angoori Bhabiji on the sets of Bhabiji Ghar Par Hain. </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >17.	When will be winner announced?</h2>
+          <p className='text-gray-500 text-sm mt-2'>Once the competition starts the winner announcement will take place every end of the month. </p>
+       </div>
+
+       <div className='section pt-4 px-6'>
+          <h2 className='text-base font-bold' >18.	How do I upload a duet video?</h2>
+          <p className='text-gray-500 text-sm mt-2'>Select the video you want to create the duet video with. Select on share option on the right side of the video. In the Share options, select on ‘Duet’. Record your video and upload it as per the instructions shared above.  </p>
+       </div>
+
     </div>
   );
 }
