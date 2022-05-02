2c82f723ab9cd76c739bedcf9f394d64ed38322f
Rishi Arya <rishi.arya@zee.com>
2022-05-02 08:14:31 +0000

    Merged in desk-profile (pull request #581)
    Desk profile
    
    * updated profile image
    
    * added dislike fix
    
    * .
    
    * latest bug fixes, shop fixes
    
    * .
    
    * .
    
    * nginx change
    
    * .
    
    * nginx changes
    
    * .

--- pages/feed-desk.js
+++ pages/feed-desk.js
@@ -1,10 +1,8 @@
 // import FeedDesk from "../src/components/feed-desk/index";
-import DeskProfile from "../src/components/desk-profile/index-old";
 export default function VideoD() {
   return (
     <div>
       {/* <FeedDesk/> */}
-      <DeskProfile/>
     </div>
       
   );
