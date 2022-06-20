a202bbd891b5c9c7bbbe8e2b8e36de1a7cdf8d6e
Cristbin Sanchez MS <cristbin.sanchez@ZL-BLR-MAC408.local>
2022-06-20 13:43:37 +0530

    noscrip-issue

--- pages/_document.js
+++ pages/_document.js
@@ -23,14 +23,7 @@ class Hipi extends Document {
     return (
       <Html lang="en">
         <Head>
-        <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          </noscript>
+       
         </Head>
         <body>
           <Main />
