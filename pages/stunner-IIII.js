aff664090914e723a1ce18dcf22a56aa19fe24d0
Rishi Arya <rishi.arya@zee.com>
2022-08-01 13:26:55 +0000

    Merged in crist-changes (pull request #800)
    .
    
    * .

---
+++ pages/stunner-IIII.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import ChooseComp from "../src/components/choose-comp";
+import { SeoMeta } from "../src/components/commons/head-meta/seo-meta";
+import { getCanonicalUrl } from "../src/utils/web";
+import Stunner from "../src/components/stunner";
+
+
+export default function Hipi() {
+
+  const [url, setUrl] = useState('');
+
+  return (
+  <>
+  <SeoMeta
+        data={{
+          title: 'Hipi Stunner 2022 Contest presented By Nikita Anand | Hipi ',
+          // image: item?.thumbnail,
+          description: 'Hipi Stunner is designed for you to celebrate your uniqueness both body and in Showcase how you carry your fashion up your sleeve and walk the big stage in style. Be a tenner, a Hipi Stunner ',
+          // openGraph: {
+          //   title: 'HiPi - Indian Short Video Platform for Fun Videos, Memes & more',
+          //   description: 'Short Video Community - Watch and create entertaining dance, romantic, funny, sad & other short videos. Find fun filters, challenges, famous celebrities and much more only on HiPi',
+          //   url: hostname || '',
+          //   images: [
+          //     {
+          //       url: item?.thumbnail,
+          //       width: 800,
+          //       height: 600,
+          //       alt: item?.music_title
+          //     },
+          //     { url: item?.userProfilePicUrl }
+          //   ],
+          //   type: 'video.movie',
+          //   video: {
+          //     actors: [
+          //       {
+          //         role: item?.userName
+          //       }
+          //     ],
+          //     tag: item?.genre
+          //   },
+          //   site_name: 'HiPi'
+          // }
+        }}
+      />
+    {/* <ChooseComp mobile={<GoatMob/>} desktop={<GoatDesk/>}/> */}
+    <Stunner/>
+  </>
+  )
+}
