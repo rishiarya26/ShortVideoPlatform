a450ce48d140c69b4f20c9d0f0ef5f747d5fd67e
Suvansh Agrawal <suvansh.agrawal@ZL-BLR-MAC097.local>
2022-08-03 14:34:25 +0530

    added footer and made css revamp

--- src/storyblokComponents/CardSummary.js
+++ src/storyblokComponents/CardSummary.js
@@ -1,61 +1,16 @@
-/*eslint-disable @next/next/no-img-element */
-import Sidebar from "../../src/storyblokComponents/SideBar";
 import Card from "../../src/storyblokComponents/Card";
-import Tab from "../../src/components/commons/tabs/newsletterTab";
-import { useRouter } from "next/router";
-import { withBasePath } from "../../src/config";
-import styles from "./cardSummary.module.css";
+import MainPage from "./MainPage";
  
-const categoriesObj = {"All": "/", "News": "news", "Product": "product", "Creator": "creator", "Business": "business"};
 export default function CardSummary({stories, heading}) {
-  const router = useRouter();
-    const pathnameExtract = (pathname) => {
-        const pathNameArray = pathname.split("/");
-        if(pathNameArray.length < 3) {
-            return "/";
-        }
-        return pathNameArray[2];
-    }
-    const currentPage = pathnameExtract(router.pathname);
-    const onChange = (page) => {
-        router.push(`/newsroom/${page}`)
-    }
+
   return (
-      <div className="flex flex-col">
-        <div className="w-full px-6 h-16 head-shadow flex justify-center items-center sticky top-0 bg-white z-20">
-          <div className="w-feed">
-          <div onClick={()=>router.push("/feed/for-you")} className="w-14 cursor-pointer">
-            <img alt="hipi logo"  src={withBasePath('icons/Logo_hipi.png')} />
-          </div>
-          </div>
-        </div>
-      <div className="flex flex-col">
-        <div className={styles.navigationContainer}>
-          <div className={styles.sideBarWrapper}>
-            <Sidebar
-              sideBarArray={categoriesObj}
-              currentPage={currentPage}
-              onChange={onChange}
-            />
-          </div>
-          <div className={styles.tabWrapper}>
-            <Tab
-              tabs={categoriesObj}
-              currentPage={currentPage}
-              onChange={onChange}
-            />
-          </div>
-          <div className={styles.cardWrapper}>
-            <div className={styles.headingWrapper}><h1 className="text-5xl font-semibold text-lg mb-8">{heading}</h1></div>
-              {stories.length > 0 ? 
-                (
-                  stories?.map((post)=><Card key={post.uuid} post={post}/>)
-                  ) : (
-                  <div className="flex items-center justify-center text-4xl h-full">No Post available</div>
-                )}
-          </div>
-        </div>
-      </div>
-    </div>
+    <MainPage heading={heading} cards={true}>
+      {stories.length > 0 ? 
+        (
+          stories?.map((post)=><Card key={post.uuid} post={post}/>)
+          ) : (
+          <div className="flex items-center justify-center text-4xl h-full">No Post available</div>
+        )}
+    </MainPage>
   )
 }
