2bda8593444db8a1226bc74758a8af73a228ed0b
Rishi Arya <rishi.arya@zee.com>
2022-08-29 12:42:00 +0000

    Merged in Business_page (pull request #873)
    Business page
    
    * pull_prod
    
    * .
    
    * .
    
    * about_mob
    
    * rish_changes
    
    * footer_mob
    
    * .
    
    * .
    
    * .
    
    * .

--- pages/ads/success-stories.js
+++ pages/ads/success-stories.js
@@ -1,5 +1,4 @@
 import { SeoMeta } from "../../src/components/commons/head-meta/seo-meta";
-import Advertisement from "../../src/components/business/advertise";
 import SuccessStories from "../../src/components/business/success";
 // import { getCanonicalUrl } from "../src/utils/web";
 
