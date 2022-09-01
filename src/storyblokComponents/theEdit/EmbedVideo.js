c353595f558fea236b93ddba61191a39dccd4b0f
Suvansh Agrawal <suvansh.agrawal@zee.com>
2022-09-01 12:43:00 +0530

    video play fix

--- src/storyblokComponents/theEdit/EmbedVideo.js
+++ src/storyblokComponents/theEdit/EmbedVideo.js
@@ -64,7 +64,7 @@ export default function Hipi({ id, type = "posterImage" }) {
   useEffect(() => {
     const videoUrl = getEffectiveVideoUrl(item.video_urls);
     setVideoUrl(videoUrl);
-  }, []);
+  }, [item]);
 
   if (resp.status === "fail") {
     return <Error message={resp.message} statusCode={resp.errorCode} />;
