31ef3b1669acc33534bf288529300025eb182196
Suvansh Agrawal <suvansh.agrawal@zee.com>
2022-09-01 12:02:18 +0530

    manual revert for prettier changes

--- src/components/hamburger-menu/index.js
+++ src/components/hamburger-menu/index.js
@@ -1,119 +1,77 @@
 /*eslint-disable @next/next/no-img-element */
 /*eslint-disable react/jsx-no-duplicate-props*/
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { withBasePath } from "../../config";
-import Burger from "../commons/svgicons/burger";
-import Close from "../commons/svgicons/close-white";
-import List from "../commons/svgicons/list";
-import PrivacyIco from "../commons/svgicons/privacy";
-import Terms from "../commons/svgicons/terms";
-import NewsroomIcon from "../commons/svgicons/newsroom";
-import Contact from "../commons/svgicons/contact";
-import GrievanceIcon from "../commons/svgicons/grievance";
-import AdsIcon from "../commons/svgicons/ads";
-import AboutIcon from "../commons/svgicons/about";
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { withBasePath } from "../../config"
+import Burger from "../commons/svgicons/burger"
+import Close from "../commons/svgicons/close-white"
+import List from "../commons/svgicons/list"
+import PrivacyIco from "../commons/svgicons/privacy"
+import Terms from "../commons/svgicons/terms"
+import NewsroomIcon from "../commons/svgicons/newsroom"
+import Contact from "../commons/svgicons/contact"
+import GrievanceIcon from "../commons/svgicons/grievance"
+import AdsIcon from "../commons/svgicons/ads"
+import AboutIcon from "../commons/svgicons/about"
 
-const HamburgerMenu = () => {
-  const [open, setOpen] = useState(false);
+const HamburgerMenu = ()=>{
+   const [open, setOpen]= useState(false);
 
-  const router = useRouter();
-  return (
-    <>
-      <div
-        onClick={() => setOpen(true)}
-        className="absolute top-0 left-4 burger mt-4 items-center flex justify-center p-4 z-20"
-      >
-        <Burger />
-      </div>
-      <div
-        className={`absolute w-3/4 h-screen top-0 bg-white z-20 transition-all duration-500 motion-reduce:transition-none ${
-          open ? "left-0" : "invisible -left-3/4"
-        }`}
-      >
-        <div
-          onClick={() => setOpen(false)}
-          className="w-full  shadow-lg flex justify-between items-center bg-hipired p-4"
-        >
-          <img
-            className="w-12 h-8"
-            src={withBasePath("images/logo_hipi.png")}
-            alt="hipi logo"
-          />
-          <Close />
-        </div>
-        <div className="flex flex-col p-4">
-          <div className="flex flex-col">
-            <div
-              onClick={() => router && router?.push("/about")}
-              className="flex items-center py-3"
-            >
-              <AboutIcon />
-              <p className="text-base px-3 text-gray-900 ">About</p>
-            </div>
-            <div
-              onClick={() => router && router?.push("/newsroom")}
-              className="flex items-center py-3"
-            >
-              <NewsroomIcon height="24px" width="24px" />
-              <p className="text-base px-3 text-gray-900 ">Newsroom</p>
-            </div>
-            <div
-              onClick={() => router && router?.push("/ads")}
-              className="flex items-center py-3"
-            >
-              <AdsIcon />
-              <p className="text-base px-3 text-gray-900 ">Ads</p>
-            </div>
-            <div
-              onClick={() =>
-                router && router?.push("/community-guidelines.html")
-              }
-              className="flex items-center py-3"
-            >
-              <List />
-              <p className="text-base px-3 text-gray-900 ">
-                Community Guidelines
-              </p>
-            </div>
-            <div
-              onClick={() => router && router?.push("/privacy-policy.html")}
-              className="flex items-center py-3"
-            >
-              <PrivacyIco />
-              <p className="text-base px-3 text-gray-900 ">Privacy Policy</p>
-            </div>
-
-            <div
-              onClick={() => router && router?.push("/terms-conditions.html")}
-              className="flex items-center py-3"
-            >
-              <Terms />
-              <p className="text-base px-3 text-gray-900 ">Terms of Use</p>
-            </div>
-            <div
-              onClick={() => router && router?.push("/grievance")}
-              className="flex items-center py-3"
-            >
-              <GrievanceIcon />
-              <p className="text-base px-3 text-gray-900 ">Grievance</p>
-            </div>
-            <div
-              onClick={() => router && router?.push("/contact-us")}
-              className="flex items-center py-3"
-            >
-              <Contact />
-              <p className="text-base px-3 text-gray-900 ">Contact us</p>
-            </div>
-            {/* <div onClick={()=>showDialog('Logout', LogoutPopup)} className="flex items-center py-3">
+    const router = useRouter();
+    return(
+        <>
+  <div onClick={()=>setOpen(true)} className="absolute top-0 left-4 burger mt-4 items-center flex justify-center p-4 z-20" >
+     <Burger/>
+  </div>
+  <div className={`absolute w-3/4 h-screen top-0 bg-white z-20 transition-all duration-500 motion-reduce:transition-none ${open ? 'left-0' : 'invisible -left-3/4' }`}>
+    <div onClick={()=>setOpen(false)} className="w-full  shadow-lg flex justify-between items-center bg-hipired p-4">
+    <img className="w-12 h-8" src={withBasePath('images/logo_hipi.png')} alt="hipi logo" /> 
+        <Close/>
+    </div>
+      <div className="flex flex-col p-4">
+        <div className="flex flex-col">
+        <div onClick={()=> router && router?.push('/about')} className="flex items-center py-3">
+          <AboutIcon/>
+            <p className="text-base px-3 text-gray-900 ">About</p>
+          </div>
+          <div onClick={()=> router && router?.push('/newsroom')} className="flex items-center py-3">
+            <NewsroomIcon height="24px" width="24px"/>
+            <p className="text-base px-3 text-gray-900 ">Newsroom</p>
+          </div>
+          <div onClick={()=> router && router?.push('/ads')} className="flex items-center py-3">
+            <AdsIcon/>
+            <p className="text-base px-3 text-gray-900 ">Ads</p>
+          </div>
+          <div onClick={()=> router && router?.push('/community-guidelines.html')} className="flex items-center py-3">
+            <List/>
+            <p className="text-base px-3 text-gray-900 ">Community Guidelines</p>
+          </div>
+          <div onClick={()=> router && router?.push('/privacy-policy.html')} className="flex items-center py-3">
+            <PrivacyIco/>
+            <p className="text-base px-3 text-gray-900 ">Privacy Policy</p>
+          </div>
+        
+          <div onClick={()=> router && router?.push('/terms-conditions.html')} className="flex items-center py-3">
+            <Terms/>
+            <p className="text-base px-3 text-gray-900 ">Terms of Use</p>
+          </div>
+          <div onClick={()=> router && router?.push('/grievance')} className="flex items-center py-3">
+            <GrievanceIcon/>
+            <p className="text-base px-3 text-gray-900 ">Grievance</p>
+          </div>
+          <div onClick={()=> router && router?.push('/contact-us')} className="flex items-center py-3">
+          <Contact/>
+            <p className="text-base px-3 text-gray-900 ">Contact us</p>
+          </div>
+          {/* <div onClick={()=>showDialog('Logout', LogoutPopup)} className="flex items-center py-3">
             <Logout/>
             <p className="text-base px-3 text-gray-900 ">Logout</p>
           </div> */}
-          </div>
         </div>
-      </div>
-    </>
-  );
-};
+    </div>
+  </div>
+</>
+    )
+}
 
 export default HamburgerMenu;
