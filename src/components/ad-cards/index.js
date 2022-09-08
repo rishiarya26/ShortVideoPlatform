44db91a43dd3fbd7d6e36e62951f7600ac7f0e9f
Suvansh Agrawal <suvansh.agrawal@zee.com>
2022-09-08 17:04:49 +0530

    ads label support added

--- src/components/ad-cards/index.js
+++ src/components/ad-cards/index.js
@@ -7,6 +7,10 @@ import useDrawer from '../../hooks/use-drawer';
 import { toTrackMixpanel } from '../../analytics/mixpanel/events';
 import { getBrand } from '../../utils/web';
 
+function LabelHolder({label}){
+  return <div className='bg-hipired text-10 rounded-2xl px-1 py-0.5 w-max absolute -bottom-2 left-1/2 transform -translate-x-1/2'>{label}</div>
+}
+
 function AdCards({
   adCards, videoId, comp, loading, pageName, tabName, campaignId="NA"
 }) {
@@ -44,19 +48,50 @@ function AdCards({
     >
           {adCardsLength > 0 && adCards.map((data, id) => (
             <div
-              key={id}
-              className="w-14 h-14 ml-2 rounded-lg bg-gray-500 overflow-hidden relative"
-              // eslint-disable-next-line no-undef
-              onClick={comp === 'feed' ? 
-              ()=>{
-                toTrackMixpanel('monetisationProductClick',{pageName:pageName, tabName:tabName},{content_id:videoId,productId:data?.card_id, productUrl:data?.product_url, brandName: getBrand(data?.product_url), campaignId})
-                window.open(data?.product_url)} :
-              () => {
-                toTrackMixpanel('monetisationProductClick',{pageName:pageName, tabName:tabName},{content_id:videoId,productId:data?.card_id, productUrl:data?.product_url, brandName: getBrand(data?.product_url), campaignId})
-                show('',charmboardDrawer , 'big', { videoId : videoId, idToScroll: data?.card_id, campaignId})}
+            key={id}
+            onClick={
+              comp === "feed"
+                ? () => {
+                    toTrackMixpanel(
+                      "monetisationProductClick",
+                      { pageName: pageName, tabName: tabName },
+                      {
+                        content_id: videoId,
+                        productId: data?.card_id,
+                        productUrl: data?.product_url,
+                        brandName: getBrand(data?.product_url),
+                        campaignId
+                      }
+                    );
+                    window.open(data?.product_url);
+                  }
+                : () => {
+                    toTrackMixpanel(
+                      "monetisationProductClick",
+                      { pageName: pageName, tabName: tabName },
+                      {
+                        content_id: videoId,
+                        productId: data?.card_id,
+                        productUrl: data?.product_url,
+                        brandName: getBrand(data?.product_url),
+                        campaignId
+                      }
+                    );
+                    show("", charmboardDrawer, "big", {
+                      videoId: videoId,
+                      idToScroll: data?.card_id,
+                    });
+                  }
             }
+            className="relative flex flex-col ml-2"
+          >
+            <div
+              className="w-14 h-14 rounded-lg bg-gray-500 overflow-hidden relative"
+              // eslint-disable-next-line no-undef
             >
               <Img data={data?.img_url} height={120} width={120} fallbakc={fallbackShop?.src}/>
+            </div>
+            {data?.card_labels && data.card_labels !== "" && <LabelHolder label={data?.card_labels}/>}
             </div>
           ))
           }
