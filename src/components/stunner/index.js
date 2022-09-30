c452a4e5773c313fc878b01dbe4a3471971266d4
Cristbin Sanchez <cristbin.sanchez@zee.com>
2022-09-30 20:52:58 +0000

    Merged in stunner_sep_30 (pull request #941)
    Stunner sep 30
    
    * stunner_oct_update
    
    * stunner_oct_update

--- src/components/stunner/index.js
+++ src/components/stunner/index.js
@@ -104,7 +104,7 @@ useEffect(()=>{
 </div>
 
 <div className='py-8 md:pt-20 w-full flex flex-col items-center justify-center'>
-<h1 className='text-3xl font-bold purple_font mb-2 px-4 text-center pb-2'> September Challenge 2 is now <span className='animate-pulse'>Live</span></h1>
+<h1 className='text-3xl font-bold purple_font mb-2 px-4 text-center pb-2'> October Challenge 1 is now <span className='animate-pulse'>Live</span></h1>
     <p className='px-8 md:w-1/2 pb-6 md:pb-6 text-center text-gray-600 font-light'>
     Create 3 different unique looks with 3 outfits.
 </p>
@@ -213,9 +213,9 @@ useEffect(()=>{
 
 
     <p className='px-8 md:px-16 md:w-1/2 pb-2 text-center text-gray-600 font-light'> 
-    Participate in both challenges of the month to win a cash prize of INR 1,00,000 and much more. Check out the Sept Challenge 2 details <span onClick={()=>challenge2Ref.current.scrollIntoView({behavior: 'smooth'})  } className='font-bold text-blue-600 cursor-pointer'>HERE</span>
+    Participate in both challenges of the month to win a cash prize of INR 1,00,000 and much more. Check out the October Challenge 1 details <span onClick={()=>challenge2Ref.current.scrollIntoView({behavior: 'smooth'})  } className='font-bold text-blue-600 cursor-pointer'>HERE</span>
     </p> <p className='px-8 md:px-16 md:w-1/2 pb-2 text-center text-gray-600 font-light'> 
-    Missed Sept Challenge 1? Not a problem! Check out the details <span onClick={()=>challenge1Ref.current.scrollIntoView({behavior: 'smooth'})  } className='font-bold text-blue-600 cursor-pointer'>HERE</span>
+    Missed Sept Challenge 2? Not a problem! Check out the details <span onClick={()=>challenge1Ref.current.scrollIntoView({behavior: 'smooth'})  } className='font-bold text-blue-600 cursor-pointer'>HERE</span>
     </p>
 
 
@@ -279,7 +279,7 @@ useEffect(()=>{
         
           <div className='flex  w-full flex-col md:flex-row justify-center flex-wrap'>
            {stunnerData.map((item,id)=>(
-           <div key={id} ref={id == 0 ? challenge1Ref : id == 1 ? challenge2Ref : null} className='w-full md:w-1/3 bg-white flex flex-col bg-white rounded-xl overflow-hidden my-4 md:mx-8 box_shadow_1 h-fit ease-in duration-300'>
+           <div key={id} ref={id == 0 ? challenge1Ref : id == 2 ? challenge2Ref : null} className='w-full md:w-1/3 bg-white flex flex-col bg-white rounded-xl overflow-hidden my-4 md:mx-8 box_shadow_1 h-fit ease-in duration-300'>
               <div className='overflow-hidden cursor-pointer  min-h-58v md:min-h-38 ' onClick={()=>item?.promoUrl&&window?.open(item?.promoUrl)}>
               <img loading="lazy"  className=''alt="Hipi Stunner Challenges " src={withBasePath(`images/stunner/${item?.imageURL}`)} />
               </div>
