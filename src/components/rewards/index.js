c92616026e3435d6f43dd55eefe0a3b1ef9952f3
Cristbin Sanchez M S <cristbin.sanchez@ZL-BLR-MAC025.local>
2022-10-03 16:50:33 +0530

    change#2

--- src/components/rewards/index.js
+++ src/components/rewards/index.js
@@ -70,25 +70,25 @@ site_name: 'Hipi'
             <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 bg_pastel_reward text-center'>
                 <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Earn rewards when you watch videos</h3>
                 <p>When you watch videos on the app, you will achieve the following levels:</p>
-                <div className='flex w-full justify-center'>
-                <div className='border border-gray-400 p-4 w-4/12 md:w-40 flex flex-col m-4'>
+                <div className='flex w-full flex-col md:flex-row justify-center items-center'>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
-                    <p className='mb-2 pl-2 font-bold text-3xl reward_font'>1</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>1</p>
                      <p className='pl-2 text-gray-500 text-sm font-medium'>On viewing 10 videos</p>
                 </div>
-                <div className='border border-gray-400 p-4 w-4/12 md:w-40 flex flex-col m-4 mx-8'>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4 md:mx-8'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
-                    <p className='mb-2 pl-2 font-bold text-3xl reward_font'>2</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>2</p>
                      <p className='pl-2 text-gray-500 text-sm font-medium'>On viewing 20 videos</p>
                 </div>
-                <div className='border border-gray-400 p-4 w-4/12 md:w-40 flex flex-col m-4'>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
-                    <p className='mb-2 pl-2 font-bold text-3xl reward_font'>3</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>3</p>
                      <p className=' pl-2 text-gray-500 text-sm font-medium'>On viewing 30 videos</p>
                 </div>
                 </div>
    
-                <p className='text-left w-full md:w-4/12 font-sm'>           
+                <p className='text-left w-full md:w-4/12 text-xs px-6'>           
                  <ul className='list-disc'>
                     <li>Videos that are watched completely will be considered. </li>
                     <li> Videos that are watched partially or skipped will not be considered. </li>
@@ -99,21 +99,56 @@ site_name: 'Hipi'
             <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 bg_1 text-center'>
                 <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Earn rewards when you follow creators</h3>
                 <p className='text-center w-full md:w-1/2'>When you follow creators on the app, you will achieve the following levels:</p>
-                <p className='mb-2 pl-2'>Level 1 – On following 20 creators</p>
-                <p className='mb-2 pl-2'>Level 2 – On following 40 creators </p>
-                <p className='mb-2 pl-2'>Level 3 – On following 60 creators</p>
-                <p className='text-center w-full md:w-1/2'>Only one follow for a creator will be considered even if you follow and unfollow the creator multiple times.</p>
+             
+                <div className='flex w-full flex-col md:flex-row justify-center items-center'>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>1</p>
+                     <p className='pl-2 text-gray-500 text-sm font-medium'>On following 20 creators</p>
+                </div>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4 md:mx-8'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>2</p>
+                     <p className='pl-2 text-gray-500 text-sm font-medium'>On following 40 creators</p>
+                </div>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-40 flex flex-col m-4'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>3</p>
+                     <p className=' pl-2 text-gray-500 text-sm font-medium'>On following 60 creators</p>
+                </div>
+                </div>
+
+
+                <p className='text-center w-full md:w-1/2 text-xs'>Only one follow for a creator will be considered even if you follow and unfollow the creator multiple times.</p>
             </div>
             <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 bg_pastel_reward text-center'>
                 <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Earn rewards when you create video and share it with your friends</h3>
                 <p className='text-center w-full md:w-1/2'>You post a video on the app and share the video link with your friends. When your friends view the video using the video link shared by you, you will achieve the following levels:</p>
-                <p className='mb-2 pl-2'>Level 1 – On receiving 5 views on the video</p>
-                <p className='mb-2 pl-2'>Level 2 – On receiving 10 views on the video </p>
-                <p className='mb-2 pl-2'>Level 3 – On receiving 15 views on the video</p>
-                <p className='text-center w-full md:w-1/2'>A new video is to be posted and shared. Videos posted or shared by you in the past will not be considered. </p>
-                <p className='text-center w-full md:w-1/2'>Views by your friends on the video through the video link shared by you will be considered. Views on the video by other users on the app will not be considered.</p>
+            
+                <div className='flex w-full flex-col md:flex-row justify-center items-center'>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-48 flex flex-col m-4'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>1</p>
+                     <p className='pl-2 text-gray-500 text-sm font-medium'>On receiving 5 views on the video</p>
+                </div>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-48 flex flex-col m-4 md:mx-8'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>2</p>
+                     <p className='pl-2 text-gray-500 text-sm font-medium'>On receiving 10 views on the video</p>
+                </div>
+                <div className='box_shadow_1 p-4 w-1/2 md:w-48 flex flex-col m-4'>
+                <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
+                    <p className='mb-2 pl-2 font-bold text-4xl reward_font'>3</p>
+                     <p className=' pl-2 text-gray-500 text-sm font-medium'>On receiving 15 views on the video</p>
+                </div>
+                </div>
+
+
+                 
+                <p className='text-left w-full md:w-1/2 text-xs'>A new video is to be posted and shared. Videos posted or shared by you in the past will not be considered. </p>
+                <p className='text-left text-xs w-full md:w-1/2'>Views by your friends on the video through the video link shared by you will be considered. Views on the video by other users on the app will not be considered.</p>
             </div>
-            <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 relative text-center'>
+            <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 relative z-0 text-center'>
                 <div className='absolute left-0 bottom-0 flex w-1/2 items-center z-10'>
                     <img loading="lazy"  alt="" className="object-contain" src={withBasePath('images/rewards/hipi_rewards_bg_2.webp')} />
                 </div>
@@ -129,9 +164,9 @@ site_name: 'Hipi'
                 <p className=''>You should spin the wheel and unlock your reward coins for the levels achieved daily before 11:59 PM during the Hipi Reward Program.</p>
                 <h3 className='mt-4 font-medium reward_font text-lg pb-1'>Terms & Conditions</h3>
                 <p>Your participation in Hipi Rewards Program is subject to further Terms & Conditions as stated below:</p>
-                <p className='mb-2 pl-2'>•	A user will be eligible for the program if the user is logged-in into Hipi mobile app on an android phone.</p>
-                <p className='mb-2 pl-2'>•	The videos posted by the user should be in line with Hipi Community Guidelines and Hipi Terms of Use. Any content that does not conform to the above-mentioned policies will be taken down from the platform. In the event of a copyright claim or any reported unauthorized use of third-party intellectual property rights, we will take down such content from the platform.</p>
-                <p className='mb-2 pl-2 mb-6'>•	Levels achieved for a particular user will be calculated based on the parameters published at the start of the Hipi Rewards Program. The calculation criteria are subject to change from time to time.</p>
+                <p className='mb-2 pl-2 text-left'>•	A user will be eligible for the program if the user is logged-in into Hipi mobile app on an android phone.</p>
+                <p className='mb-2 pl-2 text-left'>•	The videos posted by the user should be in line with Hipi Community Guidelines and Hipi Terms of Use. Any content that does not conform to the above-mentioned policies will be taken down from the platform. In the event of a copyright claim or any reported unauthorized use of third-party intellectual property rights, we will take down such content from the platform.</p>
+                <p className='mb-2 pl-2 mb-6 text-left'>•	Levels achieved for a particular user will be calculated based on the parameters published at the start of the Hipi Rewards Program. The calculation criteria are subject to change from time to time.</p>
 				</div>
             </div>
         </div>
