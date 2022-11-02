691c60c2fdb4382b82b94a85bf1df8134ea4011d
Suvansh Agrawal <suvansh.agrawal@zee.com>
2022-11-02 10:57:47 +0530

    added links to theedit

--- src/components/desk-menu/index.js
+++ src/components/desk-menu/index.js
@@ -90,6 +90,7 @@ useEffect(()=>{
         <div className="flex text-xs text-gray-400 pt-4 pr-8 flex-wrap static_links sm-menu"> 
           <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/about')}>About</div>
           <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/newsroom')}>Newsroom</div>
+          <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/theedit')}>The Edit</div>
           <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/ads')}>Ads</div>
           <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/terms-conditions.html')}>Terms of Use</div>
           <div className="mr-2 cursor-pointer mb-2 hover:border-b" onClick={()=> router && router?.push('/community-guidelines.html')} >Community Guidelines</div>
