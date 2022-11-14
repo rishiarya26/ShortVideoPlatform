f9b7639f23b10fd567b60b6e8b96963527d60163
Suvansh Agrawal <suvansh.agrawal@ZL-BLR-MAC097.local>
2022-11-14 11:31:26 +0530

    initial setup

---
+++ src/analytics/floodlight/events.js
@@ -0,0 +1,52 @@
+export const toTrackFloodlight = () => ({
+    "play": {
+        script: `
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;
+        document.write('<iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_p0;cat=ugc_p0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+        `,
+        noscript: `
+        <iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_p0;cat=ugc_p0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `
+    },
+    "replay": {
+        script: `
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;
+        document.write('<iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_r0;cat=ugc_r0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+        `,
+        noscript: `
+        <iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_r0;cat=ugc_r0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `
+    },
+    "view_5": {
+        script: `
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;
+        document.write('<iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v0;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+        `,
+        noscript: `
+        <iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v0;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `
+    },
+    "view_10": {
+        script: `
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;
+        document.write('<iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v00;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+        `,
+        noscript: `
+        <iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v00;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `
+    },
+    "view_15": {
+        script: `
+        var axel = Math.random() + "";
+        var a = axel * 10000000000000;
+        document.write('<iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v000;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+        `,
+        noscript: `
+        <iframe src="https://11937136.fls.doubleclick.net/activityi;src=11937136;type=ugc_v000;cat=ugc_v0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `
+    },
+})
