35dcf257b7361232589cc1f0ce485d0742308966
Bharat <bharat.jangid@zee.com>
2022-11-15 14:43:55 +0530

    added iphone feed

--- pages/playlist/index.js
+++ pages/playlist/index.js
@@ -1,12 +1,10 @@
 import ChooseOnType from '../../src/components/choose-on-type';
- import ProfilePlaylist from '../../src/components/profile-feed';
-import ProfileFeedIphone from '../../src/components/profile-feed-iphone';
-// import ProfileFeed from '../../src/components/profile-feed';
-// import ProfileFeedIphone from '../../src/components/profile-feed-iphone';
+import ProfilePlaylist from '../../src/components/profile-playlist';
+import ProfilePlaylistIphone from '../../src/components/profile-playlist-iphone';
 
 export default function Playlist() {
 
   return (
-    <ChooseOnType android={<ProfilePlaylist/>} ios={<ProfilePlaylist/>} />
+    <ChooseOnType android={<ProfilePlaylist/>} ios={<ProfilePlaylistIphone/>} />
   );
 }
