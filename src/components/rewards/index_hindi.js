44ef49a0fdc82a70c44337bc42b493d1584be23b
cristbinzee <cristbin.sanchez@zee.com>
2023-01-23 14:58:25 +0530

    changes#2

--- src/components/rewards/index_hindi.js
+++ src/components/rewards/index_hindi.js
@@ -67,9 +67,10 @@ site_name: 'Hipi'
     <div className=' md:mt-24'>
         <div className='flex w-full flex-col-reverse md:flex-row pb-4 md:pb-12'>
             <div className='w-full md:w-1/2 flex-col flex py-6 md:pl-40 md:p-8 px-8 justify-center '>
-                <h1 className='text-3xl text-center md:text-left reward_font font-bold font-gray-700 pb-2 mt-4 mb-2'>Earn rewards with Hipi</h1>
-                <p className='md:w-3/4'>Hipi rewards is a token of appreciation from Hipi to our incredible community of users. 
-                Hipi rewards can be earned when a user participates in regular video watching tasks. Such video watching activities are usually time bound. Thus the user must watch videos within the specified time period to earn rewards. You can earn Hipi rewards by watching videos, creating videos, and sharing the video with your friends and families. Here are some easy ways to earn Hipi rewards and a classification of the level goals to be achieved for maximum benefit: </p>
+                <h1 className='text-3xl text-center md:text-left reward_font font-bold font-gray-700 pb-2 mt-4 mb-2'>Hipi करो, Rewards जीतो</h1>
+                <p className='md:w-3/4'>Hipi Reward Program एक इनाम कार्यक्रम है। इसकी मदद से आप Hipi app पर coins जीत सकते हैं जो आपको rewards दिलाएगा। Hipi app पर रोज़ाना videos देखकर, users rewards कमा सकते हैं लेकिन videos देखने की ये गतिविधि समयबद्ध है। एक निश्चित समय के भीतर videos को देखकर, users coins इकठ्ठा कर सकते हैं। आप सिर्फ videos देखकर ही नहीं बल्कि videos बनाकर और अपने दोस्तों और परिवार के साथ videos share करके भी Hipi rewards पा सकते हैं। ऐसा करने पर, आपको ज़्यादा coins और फ़ायदे मिलेंगे। जैसे-जैसे आपका level बढ़ेगा, coins बढ़ते जाएंगे। </p>
+                <p className='md:w-3/4'>Hipi rewards कमाने के लिए, कुछ आसान तरीके आपको अधिकतम लाभ दिला सकते हैं जिसका वर्गीकरण यहां दिया गया है:</p>
+                
             </div>
             <div className=' w-full md:w-7/12 px-8 md:pr-20 pt-8 md:py-0'>
                 <img className='flex object-contain'alt="Supermonindia contest" src={withBasePath('images/rewards/hipi_rewards_sec_1.webp')} />
@@ -77,32 +78,32 @@ site_name: 'Hipi'
         </div>
         <div className="">
             <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 bg_pastel_reward text-center'>
-                <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Earn rewards when you watch videos</h3>
-                <p>When you watch videos on the app, you will unlock the following levels:</p>
+                <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Videos देखो, Rewards कमाओ </h3>
+                <p>जब आप videos देखते हैं तो Hipi app पर निम्न levels को unlock कर देंगे:</p>
                 <div className='flex w-full flex-row justify-center items-center py-4'>
                 <div className='box_shadow_1 bg-white p-4 w-1/2 md:w-40 flex flex-col m-1 md:m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>1</p>
-                     <p className='text-gray-500 text-xs md:text-sm font-medium'>On viewing 10 videos</p>
+                     <p className='text-gray-500 text-xs md:text-sm font-medium'>10 videos देखने पर</p>
                 </div>
                 <div className='box_shadow_1 bg-white p-4 w-1/2 md:w-40 flex flex-col m-1 md:m-4 md:mx-8'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>2</p>
-                     <p className='text-gray-500 text-xs md:text-sm font-medium'>On viewing 20 videos</p>
+                     <p className='text-gray-500 text-xs md:text-sm font-medium'>20 videos देखने पर</p>
                 </div>
                 <div className='box_shadow_1 bg-white p-4 w-1/2 md:w-40 flex flex-col m-1 md:m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>3</p>
-                     <p className=' text-gray-500 text-xs md:text-sm font-medium'>On viewing 30 videos</p>
+                     <p className=' text-gray-500 text-xs md:text-sm font-medium'>30 videos देखने पर</p>
                 </div>
                 </div>
    
                 <p className='text-left w-full md:w-4/12 text-xs px-6'>           
                  <ul className='list-disc'>
-                    <li>Only videos that are watched completely will be considered. </li>
-                    <li>Videos that are watched partially or skipped will not be considered. </li>
-                <li>Only one view for a video will be considered even if you watch the same video multiple times.</li>
-                <li>Your daily progress will reset at 11:59 PM every day. You should spin the wheel and unlock your coins for the levels achieved daily before 11:59 PM.</li>
+                    <li>केवल पूरी तरह देखे गए videos ही मान्य होंगे  </li>
+                    <li>आंशिक रूप से देखे गए या छोड़े गए videos मान्य नहीं होंगे </li>
+                <li>यदि आप एक ही video को कई बार देखते हैं तो भी 1 ही video गिना जायेगा</li>
+                <li>आपकी दैनिक प्रगति रोज़ रात 11:59 बजे reset हो जाएगी। हर रोज़ रात को 11:59 बजे से पहले, आप Spin The Wheel के ज़रिये coins इकठ्ठा कर के, नए levels को unlock कर पाएंगे। हरेक नया level आपको ज़्यादा से ज़्यादा rewards हासिल करने का मौका देगा।</li>
                 </ul>
                 </p>
             </div>
@@ -137,31 +138,31 @@ site_name: 'Hipi'
                 </p>
             </div> */}
             <div className='flex  flex-col items-center w-full py-8 px-4 md:py-14 bg_1 text-center'>
-                <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Earn rewards when you create a video and share it with your friends</h3>
-                <p className='text-center w-full md:w-1/2'>When you post a video on the app and share the video link with your friends. When your friends view the video using the video link shared by you, you will unlock the following levels:</p>
+                <h3 className='mt-4 font-medium text-2xl pb-1 reward_font'>Video बनाओ, Share करो और Rewards कमाओ</h3>
+                <p className='text-center w-full md:w-1/2'>आप video बना कर और उसे अपने दोस्तों के साथ share कर के, rewards हासिल कर  सकते हैं। इसके लिए आप Hipi app पर video post करने के बाद उसकी video link को अपने दोस्तों के साथ share कर दें। जब आपके मित्र आपके द्वारा share किए गए video link का उपयोग करके video देखते हैं तो आप निम्न levels को unlock कर देंगे:</p>
             
                 <div className='flex w-full justify-center items-center py-4'>
                 <div className='box_shadow_1 bg-white p-2 py-4 md:p-4 w-1/2 md:w-48 flex flex-col m-1 md:m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>1</p>
-                     <p className='text-gray-500 text-xs md:text-sm font-medium'>On receiving 5 views on the video</p>
+                     <p className='text-gray-500 text-xs md:text-sm font-medium'>Video पर 5 views मिलने पर</p>
                 </div>
                 <div className='box_shadow_1 bg-white p-2 py-4 md:p-4 w-1/2 md:w-48 flex flex-col m-1 md:m-4 md:mx-8'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>2</p>
-                     <p className='text-gray-500 text-xs md:text-sm font-medium'>On receiving 10 views on the video</p>
+                     <p className='text-gray-500 text-xs md:text-sm font-medium'>Video पर 10 views मिलने पर</p>
                 </div>
                 <div className='box_shadow_1 bg-white p-2 py-4 md:p-4 w-1/2 md:w-48 flex flex-col m-1 md:m-4'>
                 <p className=' pl-2 font-medium text-sm reward_font'>Level</p>
                     <p className='mb-2 pl-2 font-bold text-4xl reward_font'>3</p>
-                     <p className=' text-gray-500 text-xs md:text-sm font-medium'>On receiving 15 views on the video</p>
+                     <p className=' text-gray-500 text-xs md:text-sm font-medium'>Video पर 15 views मिलने पर</p>
                 </div>
                 </div>                 
                 <p className='text-left w-full md:w-4/12 text-xs px-6'>
                 <ul className='list-disc'>
-                <li>A new, original video is to be posted and shared. Videos posted or shared by you in the past will not be considered.</li>
-                <li>Views by your friends on the video through the video link shared by you only will be eligible for hipi rewards. Views on the video by other users on the app will not be considered.</li>
-                <li>Your daily progress will reset at 11:59 PM every day. You should spin the wheel and unlock your coins for the levels achieved daily before 11:59 PM.</li>
+                <li> एक नया और original video ही post और share किया जाना चाहिए। आपके द्वारा पूर्व में post या share किए गए video पर विचार नहीं किया जाएगा।</li>
+                <li>आपके द्वारा share किए गए video link के माध्यम से, जब आपके मित्र video को देखेंगे, तभी वह reward के लिए मान्य होगा। App पर अन्य users द्वारा देखे जाने वाले views, reward के लिए मान्य नहीं होंगे।</li>
+                <li>आपकी दैनिक प्रगति रोज़ रात 11:59 बजे reset हो जाएगी। हर रोज़ रात 11:59 बजे से पहले, आप Spin The Wheel के ज़रिये coins इकठ्ठा कर के नए levels को unlock कर पाएंगे। हरेक नया level आपको ज़्यादा से ज़्यादा rewards हासिल करने का मौका देगा।</li>
                 </ul>
                      </p>
             </div>
@@ -173,11 +174,11 @@ site_name: 'Hipi'
                     <img loading="lazy"  alt="" className="object-contain" src={withBasePath('images/rewards/hipi_rewards_bg_1.webp')} />
                 </div>
 				<div className='z-20 w-full md:w-3/4 flex flex-col items-center'>
-                <h3 className='mt-4 font-medium reward_font text-lg pb-4 text-center'>Unlock the levels, spin a wheel, and earn coins</h3>
-                <p>Once you unlock a level, you will see the level as ‘Unlocked’ on the app. Unlocked levels alone allow you  to spin a wheel to win coins.</p>
-                <p>You will earn coins from one of the following denominations at Level 1:</p>
+                <h3 className='mt-4 font-medium reward_font text-lg pb-4 text-center'>Wheel घुमाओ, Coins कमाओ </h3>
+                <p>जब भी आप किसी एक level को unlock कर लेते हैं तो app पर उस level को 'unlocked' के रूप में देख पाएंगे। Level unlocked होते ही, आपको एक wheel को spin करने की अनुमति मिल जाती है जिससे आप coins जीत सकते हैं।</p>
+                <p>Level 1 में आप निम्न में से किसी एक मूल्यवर्ग के coins अर्जित करेंगे:</p>
                 <p className='mb-2 py-4 font-bold text-2xl reward_font'>10, 8, 6, 4, 2, 0</p>
-                <p>These coins get multiplied by 1.5 when won at Level 2, and by 2 when won at Level 3. </p>
+                <p>Level 2 में जीतने पर ये coins 1.5 से गुणा हो जाते हैं और level 3 में जीतने पर 2 से गुणा हो जाते हैं।</p>
 
 
                 {/* <h3 className='mt-4 font-medium reward_font text-lg pb-1'>Details & Conditions</h3>
@@ -186,16 +187,16 @@ site_name: 'Hipi'
                 
                  */}
                  <div className='text-left md:w-3/4 text-sm'>
-                <h3 className='mt-6 font-medium reward_font text-lg pb-4 w-full text-center'>Terms & Conditions</h3>
+                <h3 className='mt-6 font-medium reward_font text-lg pb-4 w-full text-center'>नियम एवं शर्तें</h3>
 
-                <div className='flex items-start'>1<p className='mb-2 pl-2 text-left'> Your daily progress will reset at 11:59 PM every day during the Hipi rewards Program period</p></div>
-                <div className='flex items-start'>2<p className='mb-2 pl-2 text-left'> You should spin the wheel and unlock your coins for the levels achieved daily before 11:59 PM while the Hipi Reward Program is running.</p></div>
-                <div className='flex items-start'>3<p className='mb-2 pl-2 text-left'> A user can participate in Hipi rewards program using a single account only. If a user participates in Hipi rewards program using multiple accounts, coins cashed out will not be transferred by Hipi to the linked PayTM wallet.</p></div>
-                <div className='flex items-start'>4<p className='mb-2 pl-2 text-left'> Your participation in Hipi rewards Program is subject to further Terms & Conditions as stated below:</p></div>
+                <div className='flex items-start'>1<p className='mb-2 pl-2 text-left'> Hipi Reward Program की अवधि के दौरान आपकी दैनिक प्रगति हर रात 11:59 बजे reset हो जाएगी।</p></div>
+                <div className='flex items-start'>2<p className='mb-2 pl-2 text-left'> जब Hipi Reward Program चल रहा हो तो आपको wheel को spin करना चाहिए और रोज़ रात 11:59 बजे से पहले, unlock किए गए levels से coins इकट्ठे कर लेने चाहिए।</p></div>
+                <div className='flex items-start'>3<p className='mb-2 pl-2 text-left'> एक user केवल एक account का ही उपयोग करके Hipi Reward Program में भाग ले सकता है। यदि एक user कई accounts का उपयोग करके Hipi Reward Program में भाग लेता है तो cash out किए गए coins, Hipi द्वारा लिंक किए गए Paytm wallet में स्थानांतरित नहीं किए जाएंगे।</p></div>
+                <div className='flex items-start'>4<p className='mb-2 pl-2 text-left'>  Hipi Reward Program में आपकी भागीदारी आगे के नियमों और शर्तों के अधीन है, जैसा कि नीचे बताया गया है:</p></div>
 
-                <div className='flex items-start'>•<p className='mb-2 pl-2 text-left'> A user will be eligible for the program if the user is logged-in to Hipi mobile app  an Android phone alone.</p></div>
-                <div className='flex items-start'>•<p className='mb-2 pl-2 text-left'> The videos posted by the user should be in line with Hipi Community Guidelines and Hipi Terms of Use. Any content that does not conform to the above-mentioned policies will be taken down from the platform. In the event of a copyright claim or any reported unauthorized use of third-party intellectual property rights, we will take down such content from the platform, and the reward points would withdrawn</p></div>
-                <div className='flex items-start'>•<p className='mb-2 pl-2 mb-6 text-left'> Levels achieved for a particular user will be calculated based on the parameters published at the start of the Hipi rewards Program. The calculation criteria are subject to change from time to time.</p></div>
+                <div className='flex items-start'>•<p className='mb-2 pl-2 text-left'> एक user Hipi Reward Program के लिए तभी योग्य होगा जब उसने Hipi mobile app में log-in किसी Android फोन से किया हो।</p></div>
+                <div className='flex items-start'>•<p className='mb-2 pl-2 text-left'> User द्वारा post किए गए video, Hipi समुदाय दिशानिर्देशों और Hipi उपयोग की शर्तों के अनुरूप होने चाहिए। कोई भी सामग्री जो उपर्युक्त नीतियों के अनुरूप नहीं है, उसे platform से हटा दिया जाएगा। कॉपीराइट दावे या तृतीय-पक्ष बौद्धिक संपदा अधिकारों के किसी अनधिकृत उपयोग की सूचना मिलने की स्थिति में, हम ऐसी सामग्री को platform से हटा देंगे, और इनाम अंक वापस ले लिए जाएंगे।</p></div>
+                <div className='flex items-start'>•<p className='mb-2 pl-2 mb-6 text-left'> User के लिए प्राप्त levels की गणना Hipi Reward Program की शुरुआत में प्रकाशित मापदंडों के आधार पर की जाएगी। गणना मानदंड समय-समय पर परिवर्तन के अधीन हैं।</p></div>
 				</div>
                 </div>
             </div>
